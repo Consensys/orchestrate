7c241c4a259ec3c4a732de34e79c4c66b8bd2130
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-12-11 22:50:40 +0100

    feat@ethreum(crafter.go): implement transaction crafter

---
+++ ethereum/crafter.go
@@ -0,0 +1,82 @@
+package ethereum
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+)
+
+func bindArg(stringKind string, arg interface{}) (interface{}, error) {
+	// In current version we assume every arguments to be strings
+	a, ok := arg.(string)
+	if !ok {
+		return nil, fmt.Errorf("bindArg: expected a string but got %T", arg)
+	}
+
+	switch {
+	case stringKind == "address":
+		if !common.IsHexAddress(a) {
+			return nil, fmt.Errorf("bindArg: %q is not a valid ethereum address", a)
+		}
+		return common.HexToAddress(a), nil
+
+	case stringKind == "bytes":
+		// We do not yet support bytesx (e.g. bytes1, bytes2...)
+		return hexutil.Decode(a)
+
+	case strings.HasPrefix(stringKind, "int") || strings.HasPrefix(stringKind, "uint"):
+		// In current version we bind all types of integers to *big.Int
+		// Meaning we do not yet support int8, int16, int32, int64, uint8, uin16, uint32, uint64
+		return hexutil.DecodeBig(a)
+
+	case stringKind == "bool":
+		b, err := hexutil.DecodeBig(a)
+		if err != nil {
+			return nil, err
+		}
+		return b.Int64() > 0, nil
+
+	case strings.HasPrefix(stringKind, "string"):
+		return a, nil
+
+	// In current version we only cover basic types
+	default:
+		return nil, nil
+	}
+}
+
+// bindArgs cast string arguments to expected go-ethereum type before crafting
+func bindArgs(method abi.Method, args ...interface{}) ([]interface{}, error) {
+	if method.Inputs.LengthNonIndexed() != len(args) {
+		return nil, fmt.Errorf("BindArgs: expected %v inputs but got %v", method.Inputs.LengthNonIndexed(), len(args))
+	}
+	boundArgs := make([]interface{}, 0)
+	for i, arg := range method.Inputs.NonIndexed() {
+		boundArg, err := bindArg(arg.Type.String(), args[i])
+		if err != nil {
+			return nil, err
+		}
+		boundArgs = append(boundArgs, boundArg)
+	}
+	return boundArgs, nil
+}
+
+// CraftPayload craft a transaction payload
+func CraftPayload(method abi.Method, args ...interface{}) ([]byte, error) {
+	boundArgs, err := bindArgs(method, args...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	arguments, err := method.Inputs.Pack(boundArgs...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return append(method.Id(), arguments...), nil
+}
