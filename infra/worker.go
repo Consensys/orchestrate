8115b81e2b7af03a46060121943cc7284a27fb35
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-12-24 14:44:10 +0100

    feat@infra(worker.go): implement worker

---
+++ infra/worker.go
@@ -0,0 +1,91 @@
+package infra
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/Shopify/sarama"
+)
+
+// Worker for consuming Sarama messages
+type Worker struct {
+	handlers []HandlerFunc
+	handling sync.WaitGroup
+	pool     sync.Pool
+
+	// Channel used to limit count of goroutine handling messages
+	slots chan struct{}
+	// Channel used to indicate runner has terminated
+	done chan struct{}
+}
+
+// NewWorker creates a new worker
+// Slots indicate a count of goroutine that worker can occupy to process message
+// You must set `slots > 0`
+func NewWorker(handlers []HandlerFunc, slots uint) *Worker {
+	if slots == 0 {
+		panic(fmt.Errorf("New worker requires at least 1 goroutine slots"))
+	}
+	return &Worker{
+		handlers: handlers,
+		handling: sync.WaitGroup{},
+		pool:     sync.Pool{New: func() interface{} { return NewContext() }},
+		slots:    make(chan struct{}, slots),
+		done:     make(chan struct{}, 1),
+	}
+}
+
+// Run Starts a worker to consume sarama messages
+func (w *Worker) Run(messages <-chan *sarama.Message) {
+	for {
+		msg, ok := <-messages
+		if !ok {
+			// Message channel has been close.
+			// We wait until all messages have been properly consumed
+			w.handling.Wait()
+			// We indicate that we have gracefully stop
+			w.done <- struct{}{}
+			// Exit loop
+			break
+		} else {
+			// Acquire a goroutine slot
+			w.slots <- struct{}{}
+
+			// Execute message handling in a dedicated goroutine
+			go func(msg *sarama.Message) {
+				defer func() {
+					// Release a goroutine slot
+					<-w.slots
+				}()
+				w.handleMessage(msg)
+			}(msg)
+		}
+	}
+}
+
+func (w *Worker) handleMessage(msg *sarama.Message) {
+	// Indicate that a new message is being handled
+	w.handling.Add(1)
+
+	// Retrieve a re-cycled context
+	ctx := w.pool.Get().(*Context)
+
+	defer func(ctx *Context) {
+		// Indicate that message has been handled
+		w.handling.Done()
+
+		// Re-cycle context object
+		w.pool.Put(ctx)
+	}(ctx)
+
+	// Prepare context
+	ctx.Prepare(w.handlers, msg)
+
+	// Handle context
+	ctx.Next()
+}
+
+// Done returns a channel indicating if worker is done running
+func (w *Worker) Done() <-chan struct{} {
+	return w.done
+}
---
+++ infra/worker_test.go
@@ -0,0 +1,80 @@
+package infra
+
+import (
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/golang/protobuf/proto"
+	ethpb "gitlab.com/ConsenSys/client/fr/core-stack/core/protobuf/ethereum"
+	tracepb "gitlab.com/ConsenSys/client/fr/core-stack/core/protobuf/trace"
+	"gitlab.com/ConsenSys/client/fr/core-stack/core/types"
+)
+
+type TestHandler struct {
+	traces  []*types.Trace
+	mux     *sync.Mutex
+	handled []*Context
+}
+
+func (h *TestHandler) Handler(t *testing.T) HandlerFunc {
+	return func(ctx *Context) {
+		// We add some randomness in time execution
+		r := rand.Intn(100)
+		time.Sleep(time.Duration(r) * time.Millisecond)
+		h.mux.Lock()
+		defer h.mux.Unlock()
+		h.handled = append(h.handled, ctx)
+	}
+}
+
+func newMessage(i uint64) *sarama.Message {
+	msg := &sarama.Message{}
+	msg.Value, _ = proto.Marshal(
+		&tracepb.Trace{
+			Transaction: &ethpb.Transaction{
+				TxData: &ethpb.TxData{
+					Nonce:    i,
+					To:       "0xfF778b716FC07D98839f48DdB88D8bE583BEB684",
+					Value:    "0x2386f26fc10000",
+					Gas:      21136,
+					GasPrice: "0xee6b2800",
+					Data:     "0xabcd",
+				},
+			},
+		},
+	)
+	return msg
+}
+
+func TestWorker(t *testing.T) {
+	h := TestHandler{
+		traces:  []*types.Trace{},
+		mux:     &sync.Mutex{},
+		handled: []*Context{},
+	}
+
+	w := NewWorker([]HandlerFunc{h.Handler(t)}, 100)
+
+	// Create a Sarama message channel
+	in := make(chan *sarama.Message)
+
+	// Run worker
+	go w.Run(in)
+
+	// Feed sarama channel and then close it
+	rounds := 1000
+	for i := 1; i <= rounds; i++ {
+		in <- newMessage(uint64(i))
+	}
+	close(in)
+
+	// Wait for worker to be done
+	<-w.Done()
+
+	if len(h.handled) != rounds {
+		t.Errorf("Worker: expected %v rounds but got %v", rounds, len(h.handled))
+	}
+}
