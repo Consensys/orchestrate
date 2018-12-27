b078176b5768f79dd20e14f1f11f1a8164cb76a3
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-12-27 13:41:39 +0100

    feat@handlers(loader.go): implement TraceProtoLoader

--- handlers/loader.go
+++ handlers/loader.go
@@ -6,7 +6,19 @@ import (
 	"gitlab.com/ConsenSys/client/fr/core-stack/core/infra"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core/protobuf"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core/types"
+	tracepb "gitlab.com/ConsenSys/client/fr/core-stack/core/protobuf/trace"
 )
+
+// TraceProtoLoader return an handler loading massages in protobuf format
+func TraceProtoLoader() infra.HandlerFunc {
+	return func(ctx *infra.Context) {
+		// Cast message into a sarama.ConsumerMessage
+		ctx.Pb = ctx.Msg.(*tracepb.Trace)
+
+		// Load Trace from protobuffer
+		protobuf.LoadTrace(ctx.Pb, ctx.T)
+	}
+}
 
 // SaramaLoader return an handler to load messages from sarama
 func SaramaLoader() infra.HandlerFunc {
