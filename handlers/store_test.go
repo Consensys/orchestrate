afe886dd3d5f2676908c3f987978d1f94b2108b6
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2018-12-29 14:46:24 +0100

    test@handlers(store.go): make test concurrent

--- handlers/store_test.go
+++ handlers/store_test.go
@@ -1,73 +1,68 @@
 package handlers
 
-// import (
-// 	"sync"
-// 	"testing"
+import (
+	"fmt"
+	"math/big"
+	"sync"
+	"testing"
 
-// 	"gitlab.com/ConsenSys/client/fr/core-stack/core/infra"
-// 	tracepb "gitlab.com/ConsenSys/client/fr/core-stack/core/protobuf/trace"
-// 	"gitlab.com/ConsenSys/client/fr/core-stack/core/types"
-// )
+	"gitlab.com/ConsenSys/client/fr/core-stack/core/infra"
+	"gitlab.com/ConsenSys/client/fr/core-stack/core/types"
+)
 
-// type DummyStore struct {
-// 	mux    *sync.Mutex
-// 	stored []*types.Trace
-// }
+type MockStore struct {
+	mux    *sync.Mutex
+	stored []*types.Trace
+}
 
-// func (s *DummyStore) Store(t *types.Trace) error {
-// 	s.mux.Lock()
-// 	s.stored = append(s.stored, t)
-// 	s.mux.Unlock()
-// 	return nil
-// }
+func (s *MockStore) Store(t *types.Trace) error {
+	if t.Chain().ID.Text(10) == "0" {
+		return fmt.Errorf("Could not store")
+	}
+	s.mux.Lock()
+	s.stored = append(s.stored, t)
+	s.mux.Unlock()
+	return nil
+}
 
-// var testID = "abc"
+func MakeStoreContext(i int) *infra.Context {
+	ctx := infra.NewContext()
+	ctx.Reset()
+	switch i % 2 {
+	case 0:
+		ctx.T.Chain().ID = big.NewInt(0)
+		ctx.Keys["errors"] = 1
+	case 1:
+		ctx.T.Chain().ID = big.NewInt(10)
+		ctx.Keys["errors"] = 0
+	}
+	return ctx
+}
 
-// func newStoreTestMessage() *tracepb.Trace {
-// 	var pb tracepb.Trace
-// 	pb.Sender = &tracepb.Account{Id: testID}
-// 	return &pb
-// }
+func TestStore(t *testing.T) {
+	// Register sender handler
+	ms := MockStore{&sync.Mutex{}, []*types.Trace{}}
+	store := Store(&ms)
+	rounds := 100
+	outs := make(chan *infra.Context, rounds)
+	wg := &sync.WaitGroup{}
+	for i := 0; i < rounds; i++ {
+		wg.Add(1)
+		ctx := MakeStoreContext(i)
+		go func(ctx *infra.Context) {
+			defer wg.Done()
+			store(ctx)
+			outs <- ctx
+		}(ctx)
+	}
+	wg.Wait()
+	close(outs)
 
-// func TestStore(t *testing.T) {
-// 	// Create worker
-// 	w := infra.NewWorker(100)
+	if len(outs) != rounds {
+		t.Errorf("Store: expected %v outs but got %v", rounds, len(outs))
+	}
 
-// 	// Create Sarama loader
-// 	h := Loader(&TraceProtoUnmarshaller{})
-// 	w.Use(h)
-
-// 	// Register mock handler
-// 	mockH := NewMockHandler(50)
-// 	w.Use(mockH.Handler())
-
-// 	// Register sender handler
-// 	store := DummyStore{&sync.Mutex{}, []*types.Trace{}}
-// 	w.Use(Store(&store))
-
-// 	// Create input channel
-// 	in := make(chan interface{})
-
-// 	// Run worker
-// 	go w.Run(in)
-
-// 	// Feed input channel and then close it
-// 	rounds := 1000
-// 	for i := 1; i <= rounds; i++ {
-// 		in <- newStoreTestMessage()
-// 	}
-// 	close(in)
-
-// 	// Wait for worker to be done
-// 	<-w.Done()
-
-// 	if len(mockH.handled) != rounds {
-// 		t.Errorf("Store: expected %v rounds but got %v", rounds, len(mockH.handled))
-// 	}
-
-// 	for _, tr := range store.stored {
-// 		if tr.Sender().ID != testID {
-// 			t.Errorf("store: expected ID %v but got %v", testID, tr.Sender().ID)
-// 		}
-// 	}
-// }
+	if len(ms.stored) != rounds/2 {
+		t.Errorf("Store: expected %v stored but got %v", rounds/2, len(ms.stored))
+	}
+}
