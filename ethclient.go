f50c59f08c7c3b81ca27ec1adcb3a91305db1445
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-10 23:11:46 +0100

    feat(): port code from core to here

---
+++ ethclient.go
@@ -0,0 +1,39 @@
+package ethereum
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/rpc"
+)
+
+// EthClient embed a go-ethereum ethclient so we can implement SendRawTransaction
+type EthClient struct {
+	*ethclient.Client
+	rpc *rpc.Client
+}
+
+// NewClient creates a client that uses the given RPC client.
+func NewClient(c *rpc.Client) *EthClient {
+	ec := ethclient.NewClient(c)
+	return &EthClient{ec, c}
+}
+
+// Dial connects a client to the given URL.
+func Dial(rawurl string) (*EthClient, error) {
+	return DialContext(context.Background(), rawurl)
+}
+
+// DialContext connects a client to the given URL.
+func DialContext(ctx context.Context, rawurl string) (*EthClient, error) {
+	c, err := rpc.DialContext(ctx, rawurl)
+	if err != nil {
+		return nil, err
+	}
+	return NewClient(c), nil
+}
+
+// SendRawTransaction allows to send a raw transaction
+func (ec *EthClient) SendRawTransaction(ctx context.Context, raw string) error {
+	return ec.rpc.CallContext(ctx, nil, "eth_sendRawTransaction", raw)
+}
---
+++ signer.go
@@ -0,0 +1,81 @@
+package ethereum
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
+)
+
+// StaticSigner holds a pool of private keys in memory
+type StaticSigner struct {
+	pKeys   map[string]*ecdsa.PrivateKey
+	signers *sync.Map
+}
+
+// NewStaticSigner creates a new static signer
+func NewStaticSigner(pKeys []string) *StaticSigner {
+	s := &StaticSigner{
+		signers: &sync.Map{},
+		pKeys:   make(map[string]*ecdsa.PrivateKey),
+	}
+	for _, pKey := range pKeys {
+		prv, err := ethcrypto.HexToECDSA(pKey)
+		if err != nil {
+			panic(err)
+		}
+		a := ethcrypto.PubkeyToAddress(prv.PublicKey)
+		s.pKeys[a.Hex()] = prv
+	}
+	return s
+}
+
+// MakeSigner creates a new signer
+func MakeSigner(c *types.Chain) ethtypes.Signer {
+	var signer ethtypes.Signer
+	if c.IsEIP155 {
+		// We copy chain ID to ensure pointer can be safely used elsewhere
+		id := new(big.Int)
+		id.Set(c.ID)
+		signer = ethtypes.NewEIP155Signer(id)
+	} else {
+		signer = ethtypes.HomesteadSigner{}
+	}
+	return signer
+}
+
+func (s *StaticSigner) getSigner(c *types.Chain) ethtypes.Signer {
+	signer, _ := s.signers.LoadOrStore(c.ID.Text(16), MakeSigner(c))
+	return signer.(ethtypes.Signer)
+}
+
+// Sign sign transaction on context
+func (s *StaticSigner) Sign(chain *types.Chain, a common.Address, tx *ethtypes.Transaction) (raw []byte, hash *common.Hash, err error) {
+	signer := s.getSigner(chain)
+
+	prv, ok := s.pKeys[a.Hex()]
+	if !ok {
+		return []byte{}, nil, fmt.Errorf("No Private Key for account %q", a.Hex())
+	}
+
+	t, err := ethtypes.SignTx(tx, signer, prv)
+	if err != nil {
+		return []byte{}, nil, err
+	}
+
+	// Set raw transaction
+	raw, err = rlp.EncodeToBytes(t)
+	if err != nil {
+		// TODO: handle error
+		return []byte{}, nil, err
+	}
+	h := t.Hash()
+
+	return raw, &h, nil
+}
