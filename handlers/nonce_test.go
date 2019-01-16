cc0ba200464b864a29cfa50c5aa0e405c4c160de
Nicolas Burel <nicolas.burel@consensys.net>
2019-01-16 09:52:23 +0000

    Feature/redis nonce

--- handlers/nonce_test.go
+++ handlers/nonce_test.go
@@ -1,93 +1,76 @@
 package handlers
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"math/big"
+	"reflect"
 	"sync"
 	"testing"
 
+	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/infra"
+
 	"github.com/ethereum/go-ethereum/common"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
-	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
 )
 
-type MockNonceLocker struct {
-	t      *testing.T
-	mux    *sync.Mutex
-	value  uint64
-	status uint
-}
+const chainNonce uint64 = 42
 
-func (l *MockNonceLocker) Lock() error {
-	l.mux.Lock()
-	if l.status == 2 {
-		l.mux.Unlock()
-		return fmt.Errorf("Could not lock")
+func getChainNonce(withError bool) GetNonceFunc {
+	if withError == true {
+		return func(chainID *big.Int, a *common.Address) (uint64, error) {
+			return 0, errors.New("error")
+		}
 	}
-	return nil
-}
 
-func (l *MockNonceLocker) Unlock() error {
-	defer l.mux.Unlock()
-	if l.status == 3 {
-		return fmt.Errorf("Could not unlock")
+	return func(chainID *big.Int, a *common.Address) (uint64, error) {
+		return chainNonce, nil
 	}
-	return nil
 }
 
-func (l *MockNonceLocker) Get() (uint64, error) {
-	if l.status == 4 {
-		return 0, fmt.Errorf("Could not Get")
-	}
-	return l.value, nil
-}
-
-func (l *MockNonceLocker) Set(v uint64) error {
-	if l.status == 5 {
-		return fmt.Errorf("Could not Set")
-	}
-	return nil
-}
-
-type MockNonceManager struct {
-	t       *testing.T
-	mux     *sync.Mutex
-	counter uint
-}
-
-func (m *MockNonceManager) Obtain(chainID *big.Int, a common.Address) (services.NonceLocker, error) {
-	m.mux.Lock()
-	defer m.mux.Unlock()
-	if m.counter%6 == 1 {
-		m.counter++
-		return nil, fmt.Errorf("Could not obtain")
-	}
-	n := &MockNonceLocker{
-		t:      m.t,
-		mux:    m.mux,
-		value:  0,
-		status: m.counter % 6,
-	}
-	m.counter++
-	return n, nil
-}
-
-func makeNonceContext(i int) *types.Context {
+func makeNonceContext() *types.Context {
 	ctx := types.NewContext()
 	ctx.Reset()
 	return ctx
 }
 
-func TestNonce(t *testing.T) {
-	m := MockNonceManager{t: t, mux: &sync.Mutex{}, counter: 0}
-	nonceH := NonceHandler(&m)
+type MockNonceManager struct {
+	mux   *sync.Mutex
+	nonce uint64
+}
+
+func (nm *MockNonceManager) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return nm.nonce, true, nil
+}
+
+func (nm *MockNonceManager) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	nm.nonce = newNonce
+	return nil
+}
+
+func (nm *MockNonceManager) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	nm.mux.Lock()
+	return "random", nil
+}
+
+func (nm *MockNonceManager) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	nm.mux.Unlock()
+	return nil
+}
+
+func TestNonceHandler(t *testing.T) {
+	nm := MockNonceManager{
+		mux:   &sync.Mutex{},
+		nonce: 0,
+	}
+	nonceH := NonceHandler(&nm, getChainNonce(false))
 
 	rounds := 100
 	outs := make(chan *types.Context, rounds)
 	wg := &sync.WaitGroup{}
 	for i := 0; i < rounds; i++ {
 		wg.Add(1)
-		ctx := makeNonceContext(i)
+		ctx := makeNonceContext()
 		go func(ctx *types.Context) {
 			defer wg.Done()
 			nonceH(ctx)
@@ -99,5 +82,160 @@ func TestNonce(t *testing.T) {
 
 	if len(outs) != rounds {
 		t.Errorf("NonceHandler: expected %v outs but got %v", rounds, len(outs))
+	}
+
+	var n uint64
+	nonceSet := make(map[uint64]bool)
+	for ctx := range outs {
+		n = ctx.T.Tx().Nonce()
+		nonceSet[n] = true
+	}
+	for nonce := uint64(0); nonce < uint64(rounds); nonce++ {
+		if nonceSet[nonce] == false {
+			t.Errorf("NonceHandler: nonce %v is missing but should has been given", nonce)
+		}
+	}
+}
+
+type MockNonceManagerFromChain struct {
+	mux   *sync.Mutex
+	nonce uint64
+}
+
+func (nm *MockNonceManagerFromChain) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return nm.nonce, false, nil
+}
+
+func (nm *MockNonceManagerFromChain) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	nm.nonce = newNonce
+	return nil
+}
+
+func (nm *MockNonceManagerFromChain) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	nm.mux.Lock()
+	return "random", nil
+}
+
+func (nm *MockNonceManagerFromChain) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	nm.mux.Unlock()
+	return nil
+}
+
+func TestNonceHandlerFromChain(t *testing.T) {
+	nm := MockNonceManagerFromChain{
+		mux:   &sync.Mutex{},
+		nonce: 0,
+	}
+	nonceH := NonceHandler(&nm, getChainNonce(false))
+	ctx := makeNonceContext()
+	nonceH(ctx)
+	if ctx.T.Tx().Nonce() != chainNonce {
+		t.Errorf("NonceHandler: nonce should have come from getNonceChain")
+	}
+}
+
+type MockNonceManagerGetNonceError struct{}
+
+func (nm *MockNonceManagerGetNonceError) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return 0, true, errors.New("error")
+}
+func (nm *MockNonceManagerGetNonceError) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	return nil
+}
+func (nm *MockNonceManagerGetNonceError) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	return "random", nil
+}
+func (nm *MockNonceManagerGetNonceError) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	return nil
+}
+
+type MockNonceManagerUpdateCacheNonceError struct{}
+
+func (nm *MockNonceManagerUpdateCacheNonceError) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return 0, true, nil
+}
+func (nm *MockNonceManagerUpdateCacheNonceError) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	return errors.New("error")
+}
+func (nm *MockNonceManagerUpdateCacheNonceError) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	return "random", nil
+}
+func (nm *MockNonceManagerUpdateCacheNonceError) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	return nil
+}
+
+type MockNonceManagerGetLockError struct{}
+
+func (nm *MockNonceManagerGetLockError) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return 0, true, nil
+}
+func (nm *MockNonceManagerGetLockError) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	return nil
+}
+func (nm *MockNonceManagerGetLockError) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	return "random", errors.New("error")
+}
+func (nm *MockNonceManagerGetLockError) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	return nil
+}
+
+type MockNonceManagerGetChainNonceError struct{}
+
+func (nm *MockNonceManagerGetChainNonceError) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	return 0, false, nil
+}
+func (nm *MockNonceManagerGetChainNonceError) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	return nil
+}
+func (nm *MockNonceManagerGetChainNonceError) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	return "random", nil
+}
+func (nm *MockNonceManagerGetChainNonceError) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	return nil
+}
+
+func TestNonceHandlerErrors(t *testing.T) {
+	nonceManagers := []infra.NonceManager{
+		&MockNonceManagerGetNonceError{},
+		&MockNonceManagerUpdateCacheNonceError{},
+		&MockNonceManagerGetLockError{},
+		&MockNonceManagerGetChainNonceError{},
+	}
+
+	for _, nm := range nonceManagers {
+		var nonceH types.HandlerFunc
+		if reflect.TypeOf(nm) == reflect.TypeOf(&MockNonceManagerGetChainNonceError{}) {
+			nonceH = NonceHandler(nm, getChainNonce(true))
+		} else {
+			nonceH = NonceHandler(nm, getChainNonce(false))
+		}
+
+		ctx := makeNonceContext()
+		nonceH(ctx)
+		if len(ctx.T.Errors) == 0 {
+			t.Errorf("NonceHandler: An error should have been added to context for %T", nm)
+		}
+	}
+}
+
+type ethClientMock struct{}
+
+const nonce uint64 = 12
+
+func (ec *ethClientMock) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
+	return nonce, nil
+}
+func TestGetChainNonce(t *testing.T) {
+	client := &ethClientMock{}
+
+	cid := big.NewInt(36)
+	a := common.HexToAddress("0xabcdabcdabcdabcdabcdabcd")
+	getNonceFunc := GetChainNonce(client)
+	n, err := getNonceFunc(cid, &a)
+	if err != nil {
+		t.Error("NonceHandler: error should have been nil")
+	}
+	if n != nonce {
+		t.Errorf("NonceHandler: should have returned %v, got %v instead", nonce, n)
 	}
 }
--- infra/nonce.go
+++ infra/nonce.go
@@ -1,98 +1,189 @@
 package infra
 
 import (
-	"context"
+	"errors"
 	"fmt"
 	"math/big"
-	"sync"
+	"math/rand"
+	"strconv"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
-	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
-	"gitlab.com/ConsenSys/client/fr/core-stack/infra/striped-mutex.git"
+	"github.com/gomodule/redigo/redis"
 )
 
-// SafeNonce allow to manipulate nonce in a concurrently safe manner
-type SafeNonce struct {
-	mux   *sync.Mutex
-	value uint64
+const defaultTimeout int = 500
+
+// NonceManager is an interface for fine grain management of nonce by key
+type NonceManager interface {
+	GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error)
+	UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error
+	GetLock(chainID *big.Int, a *common.Address) (string, error)
+	ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error
 }
 
-// Lock acquire lock
-func (n *SafeNonce) Lock() error {
-	n.mux.Lock()
-	return nil
+// RedisWaitLockReleaseFunc tells what to do when waiting for the lock to be released
+type RedisWaitLockReleaseFunc func(chainID *big.Int, a *common.Address, c redis.Conn, timeout time.Duration) error
+
+// RedisNonceManager allows to manage nonce thanks to a redis base
+type RedisNonceManager struct {
+	pool            *redis.Pool
+	timeout         int                      // Timeout in millisecond for lock
+	waitLockRelease RedisWaitLockReleaseFunc // Add waitLockRelease as struct item instead of method for editing purpose in testing
 }
 
-// Unlock release lock
-func (n *SafeNonce) Unlock() error {
-	n.mux.Unlock()
-	return nil
-}
-
-// Get retrieve nonce value
-// Warning: it does not acquire the lock
-func (n *SafeNonce) Get() (uint64, error) {
-	return n.value, nil
-}
-
-// Set sets nonce value
-// Warning: it does not acquire the lock
-func (n *SafeNonce) Set(v uint64) error {
-	n.value = v
-	return nil
-}
-
-// CalibrateNonceFunc allows to calibrate managed nonce
-type CalibrateNonceFunc func(chainID *big.Int, a common.Address) (uint64, error)
-
-// CacheNonceManager allows to manage nonce
-type CacheNonceManager struct {
-	mux    *stripedmutex.StripedMutex
-	nonces *sync.Map
-
-	calibrate CalibrateNonceFunc
-}
-
-// NewCacheNonceManager creates a new cache nonce
-func NewCacheNonceManager(calibrate CalibrateNonceFunc, stripes uint) *CacheNonceManager {
-	return &CacheNonceManager{
-		mux:       stripedmutex.New(stripes),
-		nonces:    &sync.Map{},
-		calibrate: calibrate,
+// Creates a new redis pool
+func newRedisPool(port string) *redis.Pool {
+	return &redis.Pool{
+		// TODO Fine tune those parameters or make them accessible in config file
+		MaxIdle:     10000,
+		IdleTimeout: 240 * time.Second,
+		Dial:        func() (redis.Conn, error) { return redis.Dial("tcp", port) },
 	}
 }
 
-func computeKey(chainID *big.Int, a common.Address) string {
+// NewRedisNonceManager creates a new nonce cache based on redis
+func NewRedisNonceManager(redisPort string) *RedisNonceManager {
+	pool := newRedisPool(redisPort)
+	return &RedisNonceManager{
+		pool:            pool,
+		waitLockRelease: waitLockRelease,
+		timeout:         defaultTimeout,
+	}
+}
+
+// Computes a unique id for a combination of chainID + ethereum address
+func computeKey(chainID *big.Int, a *common.Address) string {
 	return fmt.Sprintf("%v-%v", chainID.Text(16), a.Hex())
 }
 
-// Obtain return a locked SafeNonce for given chain and address
-func (c *CacheNonceManager) Obtain(chainID *big.Int, a common.Address) (services.NonceLocker, error) {
-	key := computeKey(chainID, a)
-	mux, err := c.mux.GetLock(key)
-	if err != nil {
-		return nil, err
-	}
-	// Lock key
-	mux.Lock()
-	defer mux.Unlock()
-
-	// Retrieve nonce from cache
-	n, ok := c.nonces.LoadOrStore(key, &SafeNonce{mux: mux, value: 0})
-	rv := n.(*SafeNonce)
-	if !ok {
-		// If nonce has just been created we compute its initial value
-		rv.value, err = c.calibrate(chainID, a)
-		if err != nil {
-			return rv, err
-		}
-	}
-	return rv, nil
+// Computes a name for the redis lock given a chainID and an ethereum address
+func computeLockName(chainID *big.Int, a *common.Address) string {
+	return "lock-" + computeKey(chainID, a)
 }
 
-// NewEthClientNonceCalibrate returns a function to get nonce initial values from an Eth client
-func NewEthClientNonceCalibrate(ec *EthClient) CalibrateNonceFunc {
-	return func(chainID *big.Int, a common.Address) (uint64, error) {
-		return ec.PendingNonceAt(context.Background(), a)
+func toDuration(t int) time.Duration {
+	return time.Duration(t) * time.Millisecond
+}
+
+// GetNonce returns the value of the nonce (from the cache if available, from the chain otherwise)
+func (nm *RedisNonceManager) GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error) {
+	exists, err := nm.nonceInCache(chainID, a)
+	if err != nil {
+		return 0, false, err
 	}
+	if exists {
+		nonce, err := nm.getCacheNonce(chainID, a)
+		return nonce, true, err
+	}
+	return 0, false, nil
+}
+
+// Tells if the nonce is already in the cache or not
+func (nm *RedisNonceManager) nonceInCache(chainID *big.Int, a *common.Address) (bool, error) {
+	conn := nm.pool.Get()
+	defer conn.Close()
+	nonceKey := computeKey(chainID, a)
+	exists, err := redis.Bool(conn.Do("EXISTS", nonceKey))
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
+}
+
+// Get the nonce from redis
+func (nm *RedisNonceManager) getCacheNonce(chainID *big.Int, a *common.Address) (uint64, error) {
+	conn := nm.pool.Get()
+	defer conn.Close()
+	nonceKey := computeKey(chainID, a)
+	r, err := redis.Uint64(conn.Do("GET", nonceKey))
+	if err != nil {
+		return 0, err
+	}
+	return r, nil
+}
+
+// UpdateCacheNonce updates the nonce in the cache with a new value
+func (nm *RedisNonceManager) UpdateCacheNonce(chainID *big.Int, a *common.Address, newNonce uint64) error {
+	conn := nm.pool.Get()
+	defer conn.Close()
+	nonceKey := computeKey(chainID, a)
+	_, err := conn.Do("SET", nonceKey, newNonce)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetLock acquire a lock for a givne chainID and an ethereum address
+func (nm *RedisNonceManager) GetLock(chainID *big.Int, a *common.Address) (string, error) {
+	randomIntValue := rand.Int()
+	lockSig := strconv.Itoa(randomIntValue)
+	conn := nm.pool.Get()
+	defer conn.Close()
+	// TODO fix a good value for timeout after tests
+	hasLock, err := conn.Do("SET", computeLockName(chainID, a), lockSig, "NX", "PX", strconv.Itoa(nm.timeout))
+
+	if err != nil {
+		return "", err
+	}
+
+	if hasLock != "OK" {
+		err := nm.waitLockRelease(chainID, a, conn, toDuration(nm.timeout))
+		conn.Close()
+		if err != nil {
+			return "", err
+		}
+		return nm.GetLock(chainID, a)
+	}
+	return lockSig, nil
+}
+
+// wait for a signal saying that the lock has been released or for timeout
+func waitLockRelease(chainID *big.Int, a *common.Address, c redis.Conn, timeout time.Duration) error {
+	psc := redis.PubSubConn{Conn: c}
+	err := psc.PSubscribe("__keyspace@*__:" + computeLockName(chainID, a))
+	if err != nil {
+		return err
+	}
+loop:
+	for start := time.Now(); time.Since(start) < timeout; {
+		switch n := psc.ReceiveWithTimeout(timeout).(type) {
+		case redis.Message:
+			if string(n.Data) == "del" {
+				break loop
+			}
+		default:
+		}
+	}
+	return nil
+}
+
+// ReleaseLock releases a previously acquired lock
+func (nm *RedisNonceManager) ReleaseLock(chainID *big.Int, a *common.Address, lockSig string) error {
+	conn := nm.pool.Get()
+	defer conn.Close()
+	lockName := computeLockName(chainID, a)
+	raw, err := conn.Do("GET", lockName)
+	if err != nil {
+		return err
+	}
+	var value string
+	switch raw := raw.(type) {
+	case nil:
+		// The lock does not exist anymore because of timeout
+		return nil
+	case []byte:
+		value = string(raw)
+	case string:
+		value = raw
+	}
+	if value == lockSig {
+		_, err := conn.Do("DEL", lockName)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return errors.New("Lock based on another locking signature, did not unlock the lock")
 }
