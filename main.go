d9f0bc77e47ae16d46fe7edff7cdb7bf62638cc3
Nicolas Maurice <nicolas.maurice@consensys.net>
2019-01-17 02:03:38 +0100

    fix(setup.go): usage of argparse in init is incompatible with go test command

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	log "github.com/sirupsen/logrus"
+)
 
+func main() {
+	var opts Config
+	LoadConfig(&opts)
+	ConfigureLogger(opts.Log)
+	log.Infof("Start worker...")
 }
