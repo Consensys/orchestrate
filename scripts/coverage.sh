89177e86657b35d5c71eddc6734b528384fe772e
Nicolas Maurice <nicolas.maurice@consensys.net>
2019-01-17 02:00:00 +0100

    chore@scripts(coverage.sh): update for mac compatibility

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
 # Exit on error
-set -e
-
-# Make sure path is correct
-if [ ! -f "scripts/coverage.sh" ]; then
-    echo "$0 must be run from the root of the repository."
-    exit 2
-fi
-
-# Create temporary directory
-tmpdir=`mktemp -d`
+# set -e
 
 for package in $@; do
-  tmpfile=`mktemp -p ${tmpdir} --suffix .cov.tmp`
-  go test -covermode=count -coverprofile "${tmpfile}" "${package}"
+  go test -covermode=count -coverprofile=profile.out "${package}"
+  if [ -f profile.out ]; then
+    cat profile.out >> tmp.out
+    rm profile.out
+  fi
 done
 
-tmpfile=`mktemp -p ${tmpdir} --suffix .cov`
-echo "mode: count" > "${tmpfile}"
-tail -q -n +2 "${tmpdir}/"*.cov.tmp >> "${tmpfile}"
-
-go tool cover -func="${tmpfile}"
-
 # Generate coverage report in html formart
-go tool cover -html="${tmpfile}" -o coverage.html
+go tool cover -func=tmp.out
+go tool cover -html="tmp.out" -o coverage.html
 
-# Remove tempdir
-rm -R "${tmpdir}"
+rm tmp.out
