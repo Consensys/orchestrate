71f9c0e449408a7998dd2370fed7aafc28817acf
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-18 15:28:07 +0100

    fix@services(nonce_manager.go): adapt update nonce interface

--- services/nonce_manager.go
+++ services/nonce_manager.go
@@ -6,20 +6,17 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-// NonceLocker allows to safely manipulate a nonce by locking/unlocking it
-type NonceLocker interface {
-	Lock() error
-	Get() (uint64, error)
-	Set(v uint64) error
-	Unlock() error
-}
-
-// NonceManager is an interface for fine grain management of nonce by key
+// NonceManager allows to safely manipulate a nonce by locking/unlocking it
 type NonceManager interface {
-	// Return a NonceLocker
-	Obtain(chainID *big.Int, a common.Address) (NonceLocker, bool, error)
-}
+	// Get read nonce value (does not acquire lock), it should indicate if nonce was available or not
+	GetNonce(chainID *big.Int, a *common.Address) (uint64, bool, error)
 
-// NonceCalibrator is a function expected to return a calibrated nonce value
-// For example when we meet a pair chainID, address for the first time
-type NonceCalibrator func(chainID *big.Int, a common.Address) (uint64, error)
+	// Set read nonce value (does not acquire lock), it should indicate if nonce was available or not
+	SetNonce(chainID *big.Int, a *common.Address, v uint64) error
+
+	// Lock nonce
+	Lock(chainID *big.Int, a *common.Address) (string, error)
+
+	// Unlock nonce
+	Unlock(chainID *big.Int, a *common.Address) (string, error)
+}
