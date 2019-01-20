0e3218b861f4680637acc1d85a4ef02a656b156d
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-20 17:47:13 +0100

    feat(sender.go): implement mutli-chain sender

--- sender.go
+++ sender.go
@@ -2,20 +2,52 @@ package ethereum
 
 import (
 	"context"
+	"fmt"
 	"math/big"
 )
 
-// SimpleSender is a sender that can manage one chain
-type SimpleSender struct {
+// SingleChainSender is a sender that can manage one chain
+type SingleChainSender struct {
 	ec *EthClient
 }
 
-// NewSimpleSender creates a new SimpleSender
-func NewSimpleSender(ec *EthClient) *SimpleSender {
-	return &SimpleSender{ec}
+// NewSingleChainSender creates a new SingleChainSender
+func NewSingleChainSender(ec *EthClient) *SingleChainSender {
+	return &SingleChainSender{ec}
 }
 
 // Send sends transaction
-func (s *SimpleSender) Send(chainID *big.Int, raw string) error {
+func (s *SingleChainSender) Send(chainID *big.Int, raw string) error {
 	return s.ec.SendRawTransaction(context.Background(), raw)
+}
+
+// MultiChainSender is a sender that can manage one chain
+type MultiChainSender struct {
+	ecRegistry map[string]*EthClient
+}
+
+func chainIDToString(chainID *big.Int) string {
+	return chainID.Text(16)
+}
+
+// NewMultiChainSender creates a new MultiChainSender
+func NewMultiChainSender(ecs []*EthClient) *MultiChainSender {
+	ecRegistry := make(map[string]*EthClient)
+	for _, ec := range ecs {
+		chainID, err := ec.NetworkID(context.Background())
+		if err != nil {
+			panic(err)
+		}
+		ecRegistry[chainIDToString(chainID)] = ec
+	}
+	return &MultiChainSender{ecRegistry}
+}
+
+// Send sends transaction
+func (s *MultiChainSender) Send(chainID *big.Int, raw string) error {
+	ec, ok := s.ecRegistry[chainIDToString(chainID)]
+	if !ok {
+		return fmt.Errorf("Not connected to chain %v", chainID)
+	}
+	return ec.SendRawTransaction(context.Background(), raw)
 }
