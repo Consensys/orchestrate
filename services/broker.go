ae811c77150b6ec9d124557bbf61bc5bf91533ee
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-21 23:26:54 +0100

    fix@services(broker.go): update Producer interface

--- services/broker.go
+++ services/broker.go
@@ -1,8 +1,6 @@
 package services
 
-import (
-	tracepb "gitlab.com/ConsenSys/client/fr/core-stack/core.git/protobuf/trace"
-)
+import "gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
 
 // OffsetMarker is an interface to mark that a context has been processed
 // Typically marking kafka offsets of message that have been consumed
@@ -12,5 +10,5 @@ type OffsetMarker interface {
 
 // TraceProducer produces a trace in another service typically a Kafka queue
 type TraceProducer interface {
-	Produce(pb *tracepb.Trace) error
+	Produce(pb *types.Trace) error
 }
