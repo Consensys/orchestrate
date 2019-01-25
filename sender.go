e45ac5e81a296b43d14b757c2bb21492ac46e101
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-25 01:01:29 +0100

    fix(sender.go): make sender use multiclient

--- sender.go
+++ sender.go
@@ -2,52 +2,25 @@ package ethereum
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 )
 
-// SingleChainSender is a sender that can manage one chain
-type SingleChainSender struct {
-	ec *EthClient
+// TxSenderEthClient is a minimal client interface required by a TxSender
+type TxSenderEthClient interface {
+	SendRawTransaction(ctx context.Context, chainID *big.Int, raw string) error
 }
 
-// NewSingleChainSender creates a new SingleChainSender
-func NewSingleChainSender(ec *EthClient) *SingleChainSender {
-	return &SingleChainSender{ec}
+// TxSender is a sender that can manage one chain
+type TxSender struct {
+	ec TxSenderEthClient
 }
 
-// Send sends transaction
-func (s *SingleChainSender) Send(chainID *big.Int, raw string) error {
-	return s.ec.SendRawTransaction(context.Background(), raw)
-}
-
-// MultiChainSender is a sender that can manage one chain
-type MultiChainSender struct {
-	ecRegistry map[string]*EthClient
-}
-
-func chainIDToString(chainID *big.Int) string {
-	return chainID.Text(16)
-}
-
-// NewMultiChainSender creates a new MultiChainSender
-func NewMultiChainSender(ecs []*EthClient) *MultiChainSender {
-	ecRegistry := make(map[string]*EthClient)
-	for _, ec := range ecs {
-		chainID, err := ec.NetworkID(context.Background())
-		if err != nil {
-			panic(err)
-		}
-		ecRegistry[chainIDToString(chainID)] = ec
-	}
-	return &MultiChainSender{ecRegistry}
+// NewTxSender creates a new SingleChainSender
+func NewTxSender(ec TxSenderEthClient) *TxSender {
+	return &TxSender{ec}
 }
 
 // Send sends transaction
-func (s *MultiChainSender) Send(chainID *big.Int, raw string) error {
-	ec, ok := s.ecRegistry[chainIDToString(chainID)]
-	if !ok {
-		return fmt.Errorf("Not connected to chain %v", chainID)
-	}
-	return ec.SendRawTransaction(context.Background(), raw)
+func (s *TxSender) Send(chainID *big.Int, raw string) error {
+	return s.ec.SendRawTransaction(context.Background(), chainID, raw)
 }
---
+++ sender_test.go
@@ -0,0 +1,26 @@
+package ethereum
+
+import (
+	"context"
+	"math/big"
+	"sync/atomic"
+	"testing"
+)
+
+type MockTxSenderEthClient struct {
+	srtxCount uint64
+}
+
+func (ec *MockTxSenderEthClient) SendRawTransaction(ctx context.Context, chainID *big.Int, raw string) error {
+	atomic.AddUint64(&ec.srtxCount, 1)
+	return nil
+}
+
+func TestTxSender(t *testing.T) {
+	ec := MockTxSenderEthClient{}
+	txs := NewTxSender(&ec)
+	txs.Send(big.NewInt(10), "0xabcde")
+	if ec.srtxCount != 1 {
+		t.Errorf("Expected calls count to SendRawTransaction to be 1 but got %v", ec.srtxCount)
+	}
+}
