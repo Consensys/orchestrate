0c5023318aea851091e4899b5876a9abca8fc33f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-27 15:23:03 +0100

    chore@scripts(coverage.sh): update coverage.sh

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+echo "mode: count" >> tmp.out
 for package in $@; do
-  go test -covermode=count -coverprofile=profile.out "${package}"
+  go test -covermode=count -coverprofile profile.out "${package}"
   if [ -f profile.out ]; then
-    cat profile.out >> tmp.out
+    tail -q -n +2 profile.out >> tmp.out
     rm profile.out
   fi
 done
 
 # Generate coverage report in html formart
 go tool cover -func=tmp.out
-go tool cover -html="tmp.out" -o coverage.html
+go tool cover -html=tmp.out -o coverage.html
 
 # Remove temporary file
 rm tmp.out
