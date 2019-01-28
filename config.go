fe663f2b8133dcac3cbda229f3879168415bf7d1
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-28 23:12:04 +0100

    fix(config.go): update config and add tests

--- config.go
+++ config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	flags "github.com/jessevdk/go-flags"
-
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,37 +11,37 @@ type LoggerConfig struct {
 	Format string `long:"log-format" env:"LOG_FORMAT" default:"text" description:"Log formatter, one of text, json."`
 }
 
-// AppConfig application configuration
-type AppConfig struct {
-	WorkerSlots uint `short:"w" long:"worker-slots" env:"WORKER" default:"50"`
+// WorkerConfig application configuration
+type WorkerConfig struct {
+	Slots uint `short:"w" long:"worker-slots" env:"WORKER_SLOTS" default:"100" description:"Number of messages that can be treat in parallel."`
 }
 
 // KafkaConfig is the configuration of application dealing with Kafka
 type KafkaConfig struct {
-	ConsumerGroup string `short:"c" long:"consumer-group" env:"KAFKA_CONSUMER_GROUP" default:"tx-nonce-group"`
-	OutTopic      string `short:"o" long:"out-topic" env:"KAFKA_TOPIC_TX_SENDER" default:"topic-tx-sender"`
-	InTopic       string `short:"i" long:"in-topic" env:"KAFKA_TOPIC_TX_NONCE" default:"topic-tx-nonce"`
-	Address       string `long:"kafka-address" env:"KAFKA_ADDRESS" default:"localhost:9092"`
+	Address       []string `short:"k" long:"kafka-address" env:"KAFKA_ADDRESS" default:"localhost:9092" description:"Address of Kafka server to connect to"`
+	InTopic       string   `short:"i" long:"in-topic" env:"KAFKA_TOPIC_TX_NONCE" default:"topic-tx-nonce" description:"Kafka topic to consume message from"`
+	OutTopic      string   `short:"o" long:"out-topic" env:"KAFKA_TOPIC_TX_SENDER" default:"topic-tx-sender" description:"Kafka topic to send message after processing"`
+	ConsumerGroup string   `short:"g" long:"consumer-group" env:"KAFKA_NONCE_GROUP" default:"tx-nonce-group" description:"Kafka consumer group"`
 }
 
 // RedisConfig is the configuration of application dealing with Redis
 type RedisConfig struct {
-	Address     string `long:"redis-address" env:"REDIS_ADDRESS" default:"localhost:6379"`
+	Address     string `long:"redis-address" env:"REDIS_ADDRESS" default:"localhost:6379" description:"Address of Redis server to connect to"`
 	LockTimeout int    `long:"redis-lock-timeout" env:"REDIS_LOCKTIMEOUT" default:"1500"`
 }
 
 // EthConfig is the configuration of application dealing with Ethereum
 type EthConfig struct {
-	URL string `short:"e" long:"eth-client" env:"ETH_CLIENT_URL" default:"https://ropsten.infura.io/v3/81e039ce6c8a465180822b525e3644d7"`
+	URLs []string `short:"e" long:"eth-client" env:"ETH_CLIENT_URL" default:"https://ropsten.infura.io/v3/81e039ce6c8a465180822b525e3644d7"`
 }
 
 // Config worker configuration
 type Config struct {
-	Log   LoggerConfig
-	App   AppConfig
-	Kafka KafkaConfig
-	Redis RedisConfig
-	Eth   EthConfig
+	Log    LoggerConfig
+	Worker WorkerConfig
+	Kafka  KafkaConfig
+	Redis  RedisConfig
+	Eth    EthConfig
 }
 
 // LoadConfig load configuration
