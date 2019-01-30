cccc1eb62ae3d2036167b2777447632f6bd06e02
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-30 20:27:50 +0100

    fix@infra(config.go): update default config

--- infra/config.go
+++ infra/config.go
@@ -7,7 +7,7 @@ type FaucetConfig struct {
 
 	CoolDownTime string   `long:"faucet-cd-time" env:"FAUCET_COOLDOWN_TIME" default:"60s" description:"Cooldown time. Valid time units are ns, us (or µs), ms, s, m, h"`
 	BlackList    []string `long:"faucet-blacklist" env:"FAUCET_BLACKLIST" default:"3-0x7E654d251Da770A068413677967F6d3Ea2FeA9E4" description:"Blacklisted <chainID>-<Account> (e.g 3-0x7E654d251Da770A068413677967F6d3Ea2FeA9E4)"`
-	MaxBalance   string   `long:"faucet-max-balance" env:"FAUCET_MAX_BALANCE" default:"500000000000000000000" description:"Max possible balance in decimal format"`
+	MaxBalance   string   `long:"faucet-max-balance" env:"FAUCET_MAX_BALANCE" default:"200000000000000000" description:"Max possible balance in decimal format"`
 
 	Topic string `long:"faucet-topic" env:"KAFKA_TOPIC_TX_CRAFTER" default:"topic-tx-crafter" description:"Kafka topic to send credit request to"`
 }
