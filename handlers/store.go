9a82683bcd3e9c0069f5e2cb6d8933c357756b56
christian.tran <christian.tran@consensys.net>
2019-01-30 11:16:00 +0100

    feat(handlers): add common handlers

---
+++ handlers/store.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
+	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
+)
+
+// Store creates an handler that can store a trace
+func Store(store services.TraceStore) types.HandlerFunc {
+	return func(ctx *types.Context) {
+		err := store.Store(ctx.T)
+		if err != nil {
+			// TODO: handle error
+			ctx.AbortWithError(err)
+			return
+		}
+	}
+}
