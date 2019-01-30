aa04c286c2eeb5b5576f0ebb007e63a1dad17b75
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-01-30 22:54:24 +0100

    feat(config.go): implement config

--- config.go
+++ config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	flags "github.com/jessevdk/go-flags"
-
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,9 +11,29 @@ type LoggerConfig struct {
 	Format string `long:"log-format" env:"LOG_FORMAT" default:"text" description:"Log formatter, one of text, json."`
 }
 
+// WorkerConfig application configuration
+type WorkerConfig struct {
+	Slots uint `short:"w" long:"worker-slots" env:"WORKER_SLOTS" default:"100" description:"Number of messages that can be treat in parallel."`
+}
+
+// KafkaConfig is the configuration of application dealing with Kafka
+type KafkaConfig struct {
+	Address       []string `short:"k" long:"kafka-address" env:"KAFKA_ADDRESS" default:"localhost:9092" description:"Address of Kafka server to connect to"`
+	InTopic       string   `short:"i" long:"kafka-in-topic" env:"KAFKA_TOPIC_TX_SENDER" default:"topic-tx-sender" description:"Kafka topic to consume message from"`
+	ConsumerGroup string   `short:"g" long:"kafka-consumer-group" env:"KAFKA_GROUP_TX_SENDER" default:"group-tx-sender" description:"Kafka consumer group"`
+}
+
+// EthConfig is the configuration of application dealing with Ethereum
+type EthConfig struct {
+	URLs []string `short:"e" long:"eth-client" env:"ETH_CLIENT_URL" default:"https://ropsten.infura.io/v3/81e039ce6c8a465180822b525e3644d7"`
+}
+
 // Config worker configuration
 type Config struct {
-	Log LoggerConfig
+	Log    LoggerConfig
+	Worker WorkerConfig
+	Kafka  KafkaConfig
+	Eth    EthConfig
 }
 
 // LoadConfig load configuration
