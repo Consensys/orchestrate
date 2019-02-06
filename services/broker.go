6eb68ca7d833c10076e23da5aa76ba0a390389cc
christian.tran <christian.tran@consensys.net>
2019-02-06 16:20:51 +0100

    feat(Producer):  generalize Produce input without requiring to be a Trace

--- services/broker.go
+++ services/broker.go
@@ -1,14 +1,12 @@
 package services
 
-import "gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
-
 // OffsetMarker is an interface to mark that a context has been processed
 // Typically marking kafka offsets of message that have been consumed
 type OffsetMarker interface {
 	Mark(msg interface{}) error
 }
 
-// TraceProducer produces a trace in another service typically a Kafka queue
-type TraceProducer interface {
-	Produce(pb *types.Trace) error
+// Producer produces a trace in another service typically a Kafka queue
+type Producer interface {
+	Produce(pb interface{}) error
 }
