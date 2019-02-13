9d2fdb427acb3b8d1686764afd8c823d9c94c5cb
Alexandre Belling <alexandre.belling@consensys.net>
2019-02-13 15:36:21 +0100

    feature(handler): Implement the handler

--- handlers/signer.go
+++ handlers/signer.go
@@ -4,11 +4,10 @@ import (
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 // HandlerSignature implements the interface TxSigner
-func HandlerSignature(s *services.TxSigner) types.HandlerFunc {
+func HandlerSignature(s services.KeyStore) types.HandlerFunc {
 
 	return func(ctx *types.Context) {
 
@@ -16,6 +15,27 @@ func HandlerSignature(s *services.TxSigner) types.HandlerFunc {
 			return
 		}
 
-	}
+		t := ethtypes.NewTransaction(
+			ctx.T.Tx().Nonce(),
+			*ctx.T.Tx().To(),
+			ctx.T.Tx().Value(),
+			ctx.T.Tx().GasLimit(),
+			ctx.T.Tx().GasPrice(),
+			ctx.T.Tx().Data(),
+		)
 
+		// Sign transaction
+		raw, h, err := s.SignTx(ctx.T.Chain(), *ctx.T.Sender().Address, t)
+
+		if err != nil {
+			// TODO: handle error
+			ctx.AbortWithError(err)
+			return
+		}
+
+		// Update trace information
+		ctx.T.Tx().SetRaw(raw)
+		ctx.T.Tx().SetHash(h)
+
+	}
 }
