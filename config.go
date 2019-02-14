5e5c08bb19c98fe71cfd5fd88e3f46a07ae42ae0
Nicolas Burel <nicolas.burel@consensys.net>
2019-02-14 15:59:58 +0000

    fix(config): change KAFKA_GROUP_TX_CRAFTER env key

--- config.go
+++ config.go
@@ -17,7 +17,7 @@ type KafkaConfig struct {
 	Address       []string `short:"k" long:"kafka-address" env:"KAFKA_ADDRESS" default:"localhost:9092" description:"Address of Kafka server to connect to"`
 	InTopic       string   `short:"i" long:"in-topic" env:"KAFKA_TOPIC_TX_CRAFTER" default:"topic-tx-crafter" description:"Kafka topic to consume message from"`
 	OutTopic      string   `short:"o" long:"out-topic" env:"KAFKA_TOPIC_TX_NONCE" default:"topic-tx-nonce" description:"Kafka topic to send message after processing"`
-	ConsumerGroup string   `short:"g" long:"consumer-group" env:"KAFKA_CRAFTER_GROUP" default:"tx-crafter-group" description:"Kafka consumer group"`
+	ConsumerGroup string   `short:"g" long:"consumer-group" env:"KAFKA_GROUP_TX_CRAFTER" default:"tx-crafter-group" description:"Kafka consumer group"`
 }
 
 // WorkerConfig application configuration
