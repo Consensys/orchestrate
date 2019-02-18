5479041887ad2a6665aee240bdafa9f11db2a635
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-02-18 20:55:32 +0100

    feat(config): re-org config

---
+++ config/redis.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var (
+	redisAddressFlag     = "redis-address"
+	redisAddressViperKey = "redis.address"
+	redisAddressDefault  = "localhost:6379"
+	redisAddressEnv      = "REDIS_ADDRESS"
+)
+
+// RedisAddress register a flag for Redis server address
+func RedisAddress(f *pflag.FlagSet) {
+	desc := fmt.Sprintf(`Address of Redis server to connect to.
+Environment variable: %q`, redisAddressEnv)
+	f.String(redisAddressFlag, redisAddressDefault, desc)
+	viper.BindPFlag(redisAddressViperKey, f.Lookup(redisAddressFlag))
+	viper.BindEnv(redisAddressViperKey, redisAddressEnv)
+}
