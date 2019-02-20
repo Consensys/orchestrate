d3b7ca4a18dae00548ed1c6f605141c9c83aaed7
christian.tran <christian.tran@consensys.net>
2019-02-20 13:30:47 +0100

    feat(logging): log the unmarchall message and error

--- handlers/loader.go
+++ handlers/loader.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	log "github.com/sirupsen/logrus"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
 )
@@ -11,14 +10,14 @@ func Loader(u services.Unmarshaller) types.HandlerFunc {
 	return func(ctx *types.Context) {
 		// Unmarshal message
 		err := u.Unmarshal(ctx.Msg, ctx.T)
-		ctx.Logger.WithFields(log.Fields{
-			"ctx.T": ctx.T,
-		}).Debug("Unmarshal message")
 
 		if err != nil {
 			// TODO: handle error
+			ctx.Logger.Errorf("error unmarshalling: %v", err)
 			ctx.AbortWithError(err)
 			return
 		}
+
+		ctx.Logger.Debugf("message unmarshalled: %v", ctx.T)
 	}
 }
