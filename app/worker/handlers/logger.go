f693b64ac2523a0af0e9f339e0f4741b66747a3c
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-02-24 23:38:09 +0100

    fix@app(worker): clean log messages

--- app/worker/handlers/logger.go
+++ app/worker/handlers/logger.go
@@ -1,33 +1,28 @@
 package handlers
 
 import (
-	"fmt"
+	"time"
 
 	"github.com/Shopify/sarama"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
+	infSarama "gitlab.com/ConsenSys/client/fr/core-stack/infra/sarama.git"
 )
 
 // Logger to log context elements before and after the worker
 func Logger(ctx *types.Context) {
-	msg := ctx.Msg.(*sarama.ConsumerMessage)
 
-	log.WithFields(log.Fields{
-		"Offset":  msg.Offset,
-		"ChainID": ctx.T.Chain().ID.Text(10),
-		"TxHash":  ctx.T.Tx().Hash().Hex(),
-	}).Infof("Crafter [IN]\nRaw: %v\n", hexutil.Encode(ctx.T.Tx().Raw()))
+	msg := ctx.Msg.(*sarama.ConsumerMessage)
+	ctx.Logger = log.WithFields(infSarama.ConsumerMessageFields(msg))
+
+	ctx.Logger.Debug("logger: new message")
+
+	start := time.Now()
 
 	ctx.Next()
 
-	log.WithFields(log.Fields{
-		"Offset": msg.Offset,
-	}).Infof("Crafter [OUT]\nErrors: %v\n", ctx.T.Errors)
-
-	errors := ctx.T.Errors
-	if len(errors) > 0 {
-		// TODO: change to log
-		fmt.Printf("Error: %v\n", errors)
-	}
+	latency := time.Now().Sub(start)
+	ctx.Logger.WithFields(log.Fields{
+		"latency": latency,
+	}).WithError(ctx.T.Errors).Info("logger: message processed")
 }
--- app/worker/handlers/sender.go
+++ app/worker/handlers/sender.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/services"
 	"gitlab.com/ConsenSys/client/fr/core-stack/core.git/types"
@@ -18,11 +20,19 @@ func Sender(sender services.TxSender) types.HandlerFunc {
 			return
 		}
 
+		ctx.Logger = ctx.Logger.WithFields(log.Fields{
+			"chain.id": ctx.T.Chain().ID.Text(16),
+			"tx.raw": hexutil.Encode(ctx.T.Tx().Raw()),
+		})
+
 		err := sender.Send(context.Background(), ctx.T.Chain().ID, hexutil.Encode(ctx.T.Tx().Raw()))
 		if err != nil {
 			// TODO: handle error
 			ctx.AbortWithError(err)
+			ctx.Logger.WithError(err).Errorf("sender: could not send transaction")
 			return
 		}
+		ctx.Logger.WithError(err).Errorf("sender: transaction sent")
+
 	}
 }
