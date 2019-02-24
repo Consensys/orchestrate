cb13af553a6d078c9702986cf1d27ecaccdc0bee
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-02-24 23:29:09 +0100

    feat(cmd): make cobra viper app

--- cmd/run.go
+++ cmd/run.go
@@ -1,12 +1,13 @@
 package cmd
 
 import (
+	"context"
 	"os"
 
 	"github.com/spf13/cobra"
-	"gitlab.com/ConsenSys/client/fr/core-stack/boilerplate-worker.git/app"
-	"gitlab.com/ConsenSys/client/fr/core-stack/common.git/utils"
 	"gitlab.com/ConsenSys/client/fr/core-stack/common.git/config"
+	"gitlab.com/ConsenSys/client/fr/core-stack/common.git/utils"
+	"gitlab.com/ConsenSys/client/fr/core-stack/worker/tx-sender.git/app"
 )
 
 func newRunCommand() *cobra.Command {
@@ -16,22 +17,29 @@ func newRunCommand() *cobra.Command {
 		Run:   run,
 	}
 
+	// Register flags
 	config.HTTPHostname(runCmd.Flags())
+	config.EthClientURLs(runCmd.Flags())
+	config.KafkaAddresses(runCmd.Flags())
+	config.TxSenderInTopic(runCmd.Flags())
+	config.WorkerSenderGroup(runCmd.Flags())
+	config.WorkerSlots(runCmd.Flags())
 
 	return runCmd
 }
 
 func run(cmd *cobra.Command, args []string) {
 	// Create app
-	a := app.New()
+	ctx, cancel := context.WithCancel(context.Background())
+	a := app.New(ctx)
 
 	// Process signals
-	sig := utils.NewSignalListener(func(signal os.Signal) { a.Close() })
+	sig := utils.NewSignalListener(func(signal os.Signal) { cancel() })
 	defer sig.Close()
 
 	// Run App
 	a.Run()
 
-	// Wait
+	// Wait for app to properly close
 	<-a.Done()
 }
