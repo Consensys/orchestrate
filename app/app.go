698a9d6b29e004b381db04250a7fb17f3ecceaff
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-02-24 23:26:25 +0100

    feat(app): implement app

--- app/app.go
+++ app/app.go
@@ -1,97 +1,88 @@
 package app
 
 import (
+	"context"
 	"net/http"
-	"sync"
 	"sync/atomic"
-	"time"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/Shopify/sarama"
+	"github.com/spf13/viper"
+	"gitlab.com/ConsenSys/client/fr/core-stack/worker/tx-sender.git/app/infra"
 )
 
-// App boilerplate application
+// App is main application object
 type App struct {
-	dying, done chan struct{}
-	closeOnce   *sync.Once
-
 	server *http.Server
-	ready  *atomic.Value
+	infra  *infra.Infra
+
+	saramaConsumerGroup sarama.ConsumerGroup
+	saramaHandler       sarama.ConsumerGroupHandler
+
+	ctx    context.Context
+	cancel func()
+
+	ready *atomic.Value
+	done  chan struct{}
 }
 
 // New creates a new application
-func New() *App {
+func New(ctx context.Context) *App {
+	// We set a cancellable context so we can possibly abort application form within the application
+	ctx, cancel := context.WithCancel(ctx)
 	app := &App{
-		dying:     make(chan struct{}),
-		done:      make(chan struct{}),
-		closeOnce: &sync.Once{},
-		ready:     &atomic.Value{},
+		done:   make(chan struct{}),
+		infra:  infra.NewInfra(),
+		ctx:    ctx,
+		cancel: cancel,
+		ready:  &atomic.Value{},
 	}
 
-	// We are not yet ready
+	// App is not yet ready
 	app.ready.Store(false)
 
 	// Initialize app
 	app.init()
 
+	// We indicate that application is ready
+	app.ready.Store(true)
+
 	return app
 }
 
 func (app *App) init() {
-	// Initialize server on application
+	// Initialize application
 	initServer(app)
-}
-
-// Run application
-func (app *App) Run() {
-	// Run main loop
-	log.Infof("starts running...")
-	go app.run()
-}
-
-func (app *App) run() {
-	// Indicate that app is ready
-	app.ready.Store(true)
-
-	ticker := time.NewTicker(time.Second)
-appLoop:
-	for {
-		select {
-		case t := <-ticker.C:
-			log.Infof("app: %v", t)
-		case <-app.dying:
-			break appLoop
-		}
-	}
-	ticker.Stop()
-	close(app.done)
-}
-
-// Closed return whether the application has been closed
-func (app *App) Closed() bool {
-	select {
-	case <-app.dying:
-		return true
-	default:
-		return false
-	}
+	app.infra.Init()
+	initConsumerGroup(app)
 }
 
 // Ready indicate if app is ready
 func (app *App) Ready() bool {
-	return app.ready.Load().(bool)
+	select {
+	case <-app.ctx.Done():
+		return false
+	default:
+		return app.ready.Load().(bool)
+	}
 }
 
-// Close application
-func (app *App) Close() {
-	app.closeOnce.Do(func() {
-		// Indicate that app is no more ready
-		app.ready.Store(false)
-		close(app.dying)
-		log.Infof("app closing...")
-	})
+// Run application
+func (app *App) Run() {
+	// Start consumer group
+	app.saramaConsumerGroup.Consume(
+		app.ctx,
+		[]string{viper.GetString("worker.in")},
+		app.saramaHandler,
+	)
+
+	// We close infrastructure
+	app.infra.Close()
+
+	// We indicate that application has stopped running
+	close(app.done)
 }
 
-// Done return a channel indicating if application has stopped running
+// Done return a channel indicating that application is done running
 func (app *App) Done() <-chan struct{} {
 	return app.done
 }
