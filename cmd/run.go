90881d4f9077a89335f14005e7a05560b9a0b2b4
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-02-24 20:45:18 +0100

    feat@cmd(run.go): implement cobra command

--- cmd/run.go
+++ cmd/run.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
+	"context"
 	"os"
 
 	"github.com/spf13/cobra"
-	"gitlab.com/ConsenSys/client/fr/core-stack/boilerplate-worker.git/app"
+	"gitlab.com/ConsenSys/client/fr/core-stack/worker/tx-nonce.git/app"
 	"gitlab.com/ConsenSys/client/fr/core-stack/common.git/utils"
 	"gitlab.com/ConsenSys/client/fr/core-stack/common.git/config"
 )
@@ -16,22 +17,32 @@ func newRunCommand() *cobra.Command {
 		Run:   run,
 	}
 
+	// Register flags
 	config.HTTPHostname(runCmd.Flags())
+	config.EthClientURLs(runCmd.Flags())
+	config.RedisAddress(runCmd.Flags())
+	config.RedisLockTimeout(runCmd.Flags())
+	config.KafkaAddresses(runCmd.Flags())
+	config.TxNonceInTopic(runCmd.Flags())
+	config.TxSignerOutTopic(runCmd.Flags())
+	config.WorkerNonceGroup(runCmd.Flags())
+	config.WorkerSlots(runCmd.Flags())
 
 	return runCmd
 }
 
 func run(cmd *cobra.Command, args []string) {
 	// Create app
-	a := app.New()
+	ctx, cancel := context.WithCancel(context.Background())
+	a := app.New(ctx)
 
 	// Process signals
-	sig := utils.NewSignalListener(func(signal os.Signal) { a.Close() })
+	sig := utils.NewSignalListener(func(signal os.Signal) { cancel() })
 	defer sig.Close()
 
 	// Run App
 	a.Run()
 
-	// Wait
+	// Wait for app to properly close
 	<-a.Done()
 }
