99a815e50b95cdd3eb3767ed1473b5e31233dd9b
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-11 00:09:33 +0100

    fix(main.go): update import

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
-	"gitlab.com/ConsenSys/client/fr/core-stack/boilerplate-worker.git/cmd"
+	"gitlab.com/ConsenSys/client/fr/core-stack/api/context-store.git/cmd"
 )
 
 func main() {
