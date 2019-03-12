1ea27c8b77228d0db8764ba831d0a64db8cb73b4
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-12 14:00:51 +0100

    feat@common/utils(keys.go): update keys methods

--- common/utils/keys_test.go
+++ common/utils/keys_test.go
@@ -15,17 +15,17 @@ func TestKafkaChainTopic(t *testing.T) {
 func TestChainAccountKey(t *testing.T) {
 	assert.Equal(
 		t,
-		"0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@3",
+		"0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@0x3",
 		ToChainAccountKey(big.NewInt(3), common.HexToAddress("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C")),
 		"Key should match",
 	)
 
-	chainID, acc, err := FromChainAccountKey("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@3")
+	chainID, acc, err := FromChainAccountKey("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@0x3")
 
 	assert.Nil(t, err, "Should not error")
 	assert.Equal(t, int64(3), chainID.Int64(), "ChainID should be correct")
 	assert.Equal(t, "0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C", acc.Hex(), "Account should be correct")
 
-	chainID, acc, err = FromChainAccountKey("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@g3")
+	chainID, acc, err = FromChainAccountKey("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@0xg3")
 	assert.NotNil(t, err, "Should error")
 }
