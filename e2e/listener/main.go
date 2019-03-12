f847f5b8aee1d69c54df62801e1ad4ffe448b106
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-12 14:14:24 +0100

    style(e2e): clean script

--- e2e/listener/main.go
+++ e2e/listener/main.go
@@ -140,9 +140,9 @@ func main() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.DebugLevel)
 	ethURLs := []string{
-		// "https://ropsten.infura.io/v3/81e039ce6c8a465180822b525e3644d7",
-		// "https://rinkeby.infura.io/v3/bfc9d6e51fbc4d3db54bea58d1094f9c",
-		// "https://kovan.infura.io/v3/bfc9d6e51fbc4d3db54bea58d1094f9c",
+		"https://ropsten.infura.io/v3/81e039ce6c8a465180822b525e3644d7",
+		"https://rinkeby.infura.io/v3/bfc9d6e51fbc4d3db54bea58d1094f9c",
+		"https://kovan.infura.io/v3/bfc9d6e51fbc4d3db54bea58d1094f9c",
 		"https://mainnet.infura.io/v3/bfc9d6e51fbc4d3db54bea58d1094f9c",
 	}
 	log.Infof("Connecting to EthClients: %v", ethURLs)
