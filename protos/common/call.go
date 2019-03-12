acd58d4ca9345a27d94c4322f17ee73bf1e6c3f8
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-12 16:15:16 +0100

    fix@protos/common(call.go): update call

--- protos/common/call.go
+++ protos/common/call.go
@@ -2,24 +2,43 @@ package common
 
 import (
 	"fmt"
+	"regexp"
 )
 
-// StringShort returns a string representation of the method
-func (method *Method) StringShort() string {
+// Short returns a string representation of the method
+func (method *Method) Short() string {
 	if method.GetContract() == "" {
 		return ""
 	}
 
-	if method.GetVersion() == "" {
-		if method.GetDeploy() {
-			return fmt.Sprintf("deploy(%v)", method.GetContract())
-		}
+	if method.GetTag() == "" {
 		return fmt.Sprintf("%v@%v", method.GetName(), method.GetContract())
 	}
 
-	if method.GetDeploy() {
-		return fmt.Sprintf("deploy(%v[%v])", method.GetContract(), method.GetVersion())
+	return fmt.Sprintf("%v@%v[%v]", method.GetName(), method.GetContract(), method.GetTag())
+}
+
+// IsDeploy indicate wether this method for contract deployment
+func (method *Method) IsDeploy() bool {
+	return method.Name == "constructor"
+}
+
+var methodRegexp = `(?P<name>[a-zA-Z]+)@(?P<contract>[a-zA-Z0-9]+)(\[(?P<tag>[0-9a-zA-Z-\.]+)\])?`
+var methodPattern = regexp.MustCompile(methodRegexp)
+
+// FromShortMethod returns a Method object from a short String
+func FromShortMethod(s string) (*Method, error) {
+	parts := methodPattern.FindStringSubmatch(s)
+
+	if len(parts) < 3 {
+		return nil, fmt.Errorf("%v is invalid short method (expected format %q)", s, methodRegexp)
 	}
 
-	return fmt.Sprintf("%v@%v[%v]", method.GetName(), method.GetContract(), method.GetVersion())
+	name, contract, tag := parts[1], parts[2], parts[4]
+
+	return &Method{
+		Name:     name,
+		Contract: contract,
+		Tag:      tag,
+	}, nil
 }
--- protos/common/call_test.go
+++ protos/common/call_test.go
@@ -8,18 +8,35 @@ import (
 
 func TestMethod(t *testing.T) {
 	m := Method{}
-	assert.Equal(t, "", m.StringShort(), "StringShort should be empty")
+	assert.Equal(t, "", m.Short(), "Short should be empty")
 
 	m.Contract = "ERC1400"
 	m.Name = "transferByPartition"
-	assert.Equal(t, "transferByPartition@ERC1400", m.StringShort(), "StringShort should be correct")
+	assert.Equal(t, "transferByPartition@ERC1400", m.Short(), "Short should be correct")
 
-	m.Deploy = true
-	assert.Equal(t, "deploy(ERC1400)", m.StringShort(), "StringShort should be correct")
+	m.Tag = "v1.0.1"
+	assert.Equal(t, "transferByPartition@ERC1400[v1.0.1]", m.Short(), "Short should be correct")
+}
 
-	m.Version = "v1.0.1"
-	assert.Equal(t, "deploy(ERC1400[v1.0.1])", m.StringShort(), "StringShort should be correct")
+func TestFromShortMethod(t *testing.T) {
+	m, err := FromShortMethod("transfer@ERC20")
+	assert.Nil(t, err, "No error expected")
+	assert.Equal(t, "transfer", m.Name, "Name should be correct")
+	assert.Equal(t, "ERC20", m.Contract, "Contract should be correct")
+	assert.Equal(t, "", m.Tag, "Tag should be correct")
+	assert.Equal(t, false, m.IsDeploy(), "Deploy should be correct")
 
-	m.Deploy = false
-	assert.Equal(t, "transferByPartition@ERC1400[v1.0.1]", m.StringShort(), "StringShort should be correct")
+	m, err = FromShortMethod("transfer@ERC20[v1.0.1]")
+	assert.Nil(t, err, "No error expected")
+	assert.Equal(t, "transfer", m.Name, "Name should be correct")
+	assert.Equal(t, "ERC20", m.Contract, "Contract should be correct")
+	assert.Equal(t, "v1.0.1", m.Tag, "Tag should be correct")
+	assert.Equal(t, false, m.IsDeploy(), "Deploy should be correct")
+
+	m, err = FromShortMethod("constructor@ERC20[v1.0.1]")
+	assert.Nil(t, err, "No error expected")
+	assert.Equal(t, "constructor", m.Name, "Name should be correct")
+	assert.Equal(t, "ERC20", m.Contract, "Contract should be correct")
+	assert.Equal(t, "v1.0.1", m.Tag, "Tag should be correct")
+	assert.Equal(t, true, m.IsDeploy(), "Deploy should be correct")
 }
