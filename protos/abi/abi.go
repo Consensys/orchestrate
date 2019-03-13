489eb7e814b0ecb12fb0c9bd3f7edbaad6b4360f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-13 18:49:45 +0100

    feat@protos/common(abi.go): implement StringToContract with abi & bytecode support

--- protos/abi/abi.go
+++ protos/abi/abi.go
@@ -5,9 +5,10 @@ import (
 	"regexp"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-// Short returns a string representation of the method
+// Short returns a short string representation of contract information
 func (c *Contract) Short() string {
 	if c.GetName() == "" {
 		return ""
@@ -20,32 +21,58 @@ func (c *Contract) Short() string {
 	return fmt.Sprintf("%v[%v]", c.GetName(), c.GetTag())
 }
 
-var contractRegexp = `^(?P<contract>[a-zA-Z0-9]+)(\[(?P<tag>[0-9a-zA-Z-.]+)\])?$`
+// Long return a long string representation of contract information
+func (c *Contract) Long() string {
+	return fmt.Sprintf("%v:%v:%v", c.Short(), string(c.Abi), string(c.Bytecode))
+}
+
+var contractRegexp = `^(?P<contract>[a-zA-Z0-9]+)(\[(?P<tag>[0-9a-zA-Z-.]+)\])?(:(?P<abi>\[.+\]))?(:(?P<bytecode>0[xX][a-fA-F0-9]+))?$`
 var contractPattern = regexp.MustCompile(contractRegexp)
 
-// FromShortContract computes a Contract from is short representation
-func FromShortContract(s string) (*Contract, error) {
+// StringToContract computes a Contract from is short representation
+func StringToContract(s string) (*Contract, error) {
 	parts := contractPattern.FindStringSubmatch(s)
 
-	if len(parts) != 4 {
-		return nil, fmt.Errorf("%v is invalid short method (expected format %q)", s, contractRegexp)
+	if len(parts) != 8 {
+		return nil, fmt.Errorf("String format invalid (expected format %q): %q", contractRegexp, s)
 	}
 
-	name, tag := parts[1], parts[3]
+	c := &Contract{
+		Name: parts[1],
+		Tag:  parts[3],
+	}
 
-	return &Contract{
-		Name: name,
-		Tag:  tag,
-	}, nil
+	// Make sure bytecode is valid and set bytecode
+	if parts[7] == "" {
+		parts[7] = "0x"
+	}
+	bytecode, err := hexutil.Decode(parts[7])
+	if err != nil {
+		return nil, fmt.Errorf("Contract %q bytecode is invalid", c.Short())
+	}
+	c.Bytecode = bytecode
+
+	// Set ABI and make sure it is valid
+	c.Abi = []byte(parts[5])
+	_, err = c.ToABI()
+	if err != nil {
+		return nil, fmt.Errorf("Contract %q ABI is invalid", c.Short())
+	}
+
+	return c, nil
 }
 
 // ToABI returns a Geth ABI object built from a contract ABI
 func (c *Contract) ToABI() (*abi.ABI, error) {
 	abi := &abi.ABI{}
-	err := abi.UnmarshalJSON(c.Abi)
-	if err != nil {
-		return nil, err
+
+	if len(c.Abi) > 0 {
+		err := abi.UnmarshalJSON(c.Abi)
+		if err != nil {
+			return nil, err
+		}
 	}
+
 	return abi, nil
 }
 
--- protos/abi/abi_test.go
+++ protos/abi/abi_test.go
@@ -17,17 +17,40 @@ func TestContract(t *testing.T) {
 	assert.Equal(t, "ERC1400[v1.0.1]", c.Short(), "Short should be correct")
 }
 
-func TestFromShortContract(t *testing.T) {
-	c, err := FromShortContract("ERC20")
+func TestFromStringd(t *testing.T) {
+	c, err := StringToContract("ERC20")
 	assert.Nil(t, err, "No error expected")
 	assert.Equal(t, "ERC20", c.Name, "Contract should be correct")
 	assert.Equal(t, "", c.Tag, "Tag should be correct")
+	assert.Equal(t, []byte{}, c.Abi, "ABI should be correct")
+	assert.Equal(t, []byte{}, c.Bytecode, "Bytecode should be correct")
 
-	c, err = FromShortContract("ERC20[v0.1.2-alpha]")
+	c, err = StringToContract("ERC20[v0.1.2-alpha]")
 	assert.Nil(t, err, "No error expected")
 	assert.Equal(t, "ERC20", c.Name, "Contract should be correct")
 	assert.Equal(t, "v0.1.2-alpha", c.Tag, "Tag should be correct")
+	assert.Equal(t, []byte{}, c.Abi, "ABI should be correct")
+	assert.Equal(t, []byte{}, c.Bytecode, "Bytecode should be correct")
 
-	c, err = FromShortContract("ERC20[v0.1.2;alpha]")
+	c, err = StringToContract("ERC20[v0.1.2-alpha]:[{\"constant\":true,\"inputs\":[],\"name\":\"testMethod\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]")
+	assert.Nil(t, err, "No error expected")
+	assert.Equal(t, "ERC20", c.Name, "Contract should be correct")
+	assert.Equal(t, "v0.1.2-alpha", c.Tag, "Tag should be correct")
+	assert.NotEmpty(t, c.Abi, "ABI should have been registered")
+	assert.Equal(t, []byte{}, c.Bytecode, "Bytecode should be correct")
+
+	c, err = StringToContract("ERC20[v0.1.2-alpha]:[{\"constant\":true,\"inputs\":[],\"name\":\"testMethod\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]:0xabcd1234ef")
+	assert.Nil(t, err, "No error expected")
+	assert.Equal(t, "ERC20", c.Name, "Contract should be correct")
+	assert.Equal(t, "v0.1.2-alpha", c.Tag, "Tag should be correct")
+	assert.NotEmpty(t, c.Abi, "ABI should have been registered")
+	assert.Equal(t, []byte{0xab, 0xcd, 0x12, 0x34, 0xef}, c.Bytecode, "Bytecode should be correct")
+
+	gethABI, err := c.ToABI()
+	assert.Nil(t, err, "ABI has been properly parsed")
+	assert.Len(t, gethABI.Methods, 1, "Method has been registered")
+	assert.Equal(t, "testMethod", gethABI.Methods["testMethod"].Name, "method name should match")
+
+	c, err = StringToContract("ERC20[v0.1.2;alpha]")
 	assert.NotNil(t, err, "Expected error")
 }
