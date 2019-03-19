8c8617586d2b0167a45f757ad9d1c91f0ae76796
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-20 00:56:01 +0100

    fix@e2e(producer): update producer with identifiers

--- e2e/producer/main.go
+++ e2e/producer/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"github.com/Shopify/sarama"
 	"github.com/golang/protobuf/proto"
-	commonpb "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/common"
-	ethpb "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/ethereum"
-	tracepb "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/trace"
+	common "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/common"
+	ethereum "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/ethereum"
+	trace "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/trace"
 )
 
 var (
@@ -15,18 +16,32 @@ var (
 	topic    = "topic-tx-sender"
 )
 
+var letterRunes = []rune("abcdef0123456789")
+
+// RandString creates a random string
+func RandString(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
+
 func newMessage(i int) *sarama.ProducerMessage {
 	msg := &sarama.ProducerMessage{
 		Topic:     topic,
 		Partition: -1,
 	}
 	b, _ := proto.Marshal(
-		&tracepb.Trace{
-			Chain: &commonpb.Chain{Id: "0x3"},
-			Tx: &ethpb.Transaction{
-				TxData: &ethpb.TxData{Nonce: 1, To: "0xfF778b716FC07D98839f48DdB88D8bE583BEB684", Value: "0x2386f26fc10000", Gas: 21136, GasPrice: "0xee6b2800", Data: "0xabcd"},
+		&trace.Trace{
+			Chain: &common.Chain{Id: "0x3"},
+			Tx: &ethereum.Transaction{
+				TxData: &ethereum.TxData{Nonce: 1, To: "0xfF778b716FC07D98839f48DdB88D8bE583BEB684", Value: "0x2386f26fc10000", Gas: 21136, GasPrice: "0xee6b2800", Data: "0xabcd"},
 				Raw:    "0xf86c0184ee6b280082529094ff778b716fc07d98839f48ddb88d8be583beb684872386f26fc1000082abcd29a0d1139ca4c70345d16e00f624622ac85458d450e238a48744f419f5345c5ce562a05bd43c512fcaf79e1756b2015fec966419d34d2a87d867b9618a48eca33a1a80",
-				Hash:   "0xbf0b3048242aff8287d1dd9de0d2d100cee25d4ea45b8afa28bdfc1e2a775afd",
+				Hash:   "0x" + RandString(32),
+			},
+			Metadata: &trace.Metadata{
+				Id: RandString(32),
 			},
 		},
 	)
@@ -41,7 +56,6 @@ func main() {
 	config.Consumer.Return.Errors = true
 
 	// Create client
-
 	client, err := sarama.NewClient(kafkaURL, config)
 	if err != nil {
 		fmt.Println(err)
