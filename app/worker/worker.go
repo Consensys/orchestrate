530f9272ec752c94d2ed2a196f3379a6562b6932
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-03-20 01:47:32 +0100

    fix@app/worker(worker.go): add partitionning key

--- app/worker/worker.go
+++ app/worker/worker.go
@@ -1,7 +1,9 @@
 package worker
 
 import (
-	commonHandlers "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/common/handlers"
+	"github.com/Shopify/sarama"
+
+	commonhandlers "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/common/handlers"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/core/services"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/core/worker"
 	"gitlab.com/ConsenSys/client/fr/core-stack/worker/tx-sender.git/app/infra"
@@ -12,15 +14,16 @@ import (
 func CreateWorker(infra *infra.Infra, marker services.OffsetMarker) *worker.Worker {
 	// Instantiate worker
 	w := worker.NewWorker(worker.NewConfig())
+	w.Partitionner(func(msg interface{}) []byte { return msg.(*sarama.ConsumerMessage).Key })
 
 	// Handler::loader
-	w.Use(commonHandlers.Loader(infra.Unmarshaller))
+	w.Use(commonhandlers.Loader(infra.Unmarshaller))
 
 	// Handler::logger
 	w.Use(handlers.Logger)
 
 	// Handler::marker
-	w.Use(commonHandlers.Marker(marker))
+	w.Use(commonhandlers.Marker(marker))
 
 	// Handler::Sender
 	w.Use(handlers.Sender(infra.TxSender, infra.Store))
