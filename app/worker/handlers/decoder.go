ad2905838843261afb279316e2ef59fea639f35c
christian.tran <christian.tran@consensys.net>
2019-03-25 11:00:49 +0100

    feat: Add event sig in proto msg

--- app/worker/handlers/decoder.go
+++ app/worker/handlers/decoder.go
@@ -45,6 +45,7 @@ func Decoder(r services.ABIRegistry) worker.HandlerFunc {
 
 			// Set decoded data on log
 			l.DecodedData = mapping
+			l.Event = l.Topics[0]
 
 			ctx.Logger.WithFields(log.Fields{
 				"log": mapping,
