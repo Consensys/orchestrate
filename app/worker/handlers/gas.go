514979f1058634c716e60cc6b9e915fab4d4f24a
christian.tran <christian.tran@consensys.net>
2019-03-30 00:27:54 +0100

    fix: not retrieve gas and gasPrice after ctx.next()

--- app/worker/handlers/gas.go
+++ app/worker/handlers/gas.go
@@ -15,21 +15,19 @@ func GasPricer(p services.GasPricer) worker.HandlerFunc {
 	return func(ctx *worker.Context) {
 
 		if ctx.T.GetTx().GetTxData().GetGasPrice() != "" {
-			ctx.Next()
-		}
-
-		p, err := p.SuggestGasPrice(ctx.Context(), ctx.T.Chain.ID())
-		if err != nil {
-			// TODO: handle error
-			ctx.Logger.WithError(err).Errorf("gas-pricer: could not suggest gas price")
-			ctx.AbortWithError(err)
-		} else {
-			ctx.T.Tx.TxData.SetGasPrice(p)
-			// Enrich logger
-			ctx.Logger = ctx.Logger.WithFields(log.Fields{
-				"tx.gas.price": p.Text(10),
-			})
-			ctx.Logger.Debugf("gas-pricer: gas price set")
+			p, err := p.SuggestGasPrice(ctx.Context(), ctx.T.Chain.ID())
+			if err != nil {
+				// TODO: handle error
+				ctx.Logger.WithError(err).Errorf("gas-pricer: could not suggest gas price")
+				ctx.AbortWithError(err)
+			} else {
+				ctx.T.Tx.TxData.SetGasPrice(p)
+				// Enrich logger
+				ctx.Logger = ctx.Logger.WithFields(log.Fields{
+					"tx.gas.price": p.Text(10),
+				})
+				ctx.Logger.Debugf("gas-pricer: gas price set")
+			}
 		}
 	}
 }
@@ -43,37 +41,34 @@ func GasEstimator(p services.GasEstimator) worker.HandlerFunc {
 
 	return func(ctx *worker.Context) {
 
-		if ctx.T.GetTx().GetTxData().GetGas() != 0 {
-			ctx.Next()
+		if ctx.T.GetTx().GetTxData().GetGas() == 0 {
+			// Retrieve re-cycled CallMsg
+			call := pool.Get().(ethereum.CallMsg)
+			defer pool.Put(call)
+
+			To, _ := ctx.T.Tx.GetTxData().ToAddress()
+			From, _ := ctx.T.GetSender().Address()
+			// Set CallMsg
+			call.From = From
+			call.To = &To
+			call.Value = ctx.T.GetTx().GetTxData().ValueBig()
+			call.Data = ctx.T.GetTx().GetTxData().DataBytes()
+
+			g, err := p.EstimateGas(ctx.Context(), ctx.T.GetChain().ID(), call)
+			if err != nil {
+				// TODO: handle error
+				ctx.Logger.WithError(err).Errorf("gas-estimator: could not estimate gas limit")
+				ctx.AbortWithError(err)
+			} else {
+				// Set gas limit on context
+				ctx.T.GetTx().GetTxData().SetGas(g)
+
+				// Enrich logger
+				ctx.Logger = ctx.Logger.WithFields(log.Fields{
+					"tx.gas": g,
+				})
+				ctx.Logger.Debugf("gas-estimator: gas limit set")
+			}
 		}
-
-		// Retrieve re-cycled CallMsg
-		call := pool.Get().(ethereum.CallMsg)
-		defer pool.Put(call)
-
-		To, _ := ctx.T.Tx.GetTxData().ToAddress()
-		From, _ := ctx.T.GetSender().Address()
-		// Set CallMsg
-		call.From = From
-		call.To = &To
-		call.Value = ctx.T.GetTx().GetTxData().ValueBig()
-		call.Data = ctx.T.GetTx().GetTxData().DataBytes()
-
-		g, err := p.EstimateGas(ctx.Context(), ctx.T.GetChain().ID(), call)
-		if err != nil {
-			// TODO: handle error
-			ctx.Logger.WithError(err).Errorf("gas-estimator: could not estimate gas limit")
-			ctx.AbortWithError(err)
-		} else {
-			// Set gas limit on context
-			ctx.T.GetTx().GetTxData().SetGas(g)
-
-			// Enrich logger
-			ctx.Logger = ctx.Logger.WithFields(log.Fields{
-				"tx.gas": g,
-			})
-			ctx.Logger.Debugf("gas-estimator: gas limit set")
-		}
-
 	}
 }
