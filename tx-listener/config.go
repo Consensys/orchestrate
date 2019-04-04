a509b01274d7be5d232f0b5eec7002c9a76cd1ad
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-04 05:34:03 +0200

    fix@tx-listener(config.go): clean configuration declaration

--- tx-listener/config.go
+++ tx-listener/config.go
@@ -9,9 +9,12 @@ import (
 )
 
 func init() {
-	viper.SetDefault("listener.block.limit", 20)
-	viper.SetDefault("listener.block.backoff", time.Second)
-	viper.SetDefault("listener.tracker.depth", 0)
+	viper.SetDefault(blockBackoffViperKey, blockBackoffDefault)
+	viper.BindEnv(blockBackoffViperKey, blockBackoffEnv)
+	viper.SetDefault(blockLimitViperKey, blockLimitDefault)
+	viper.BindEnv(blockLimitViperKey, blockLimitEnv)
+	viper.SetDefault(trackerDepthViperKey, trackerDepthDefault)
+	viper.BindEnv(trackerDepthViperKey, trackerDepthEnv)
 	viper.SetDefault("ethclient.retry.initinterval", 500*time.Millisecond)
 	viper.SetDefault("ethclient.retry.randomfactor", 0.5)
 	viper.SetDefault("ethclient.retry.multiplier", 1.5)
@@ -94,9 +97,7 @@ func BlockBackoff(f *pflag.FlagSet) {
 	desc := fmt.Sprintf(`Backoff time to wait before retrying after failing to find a mined block
 Environment variable: %q`, blockBackoffEnv)
 	f.Duration(blockBackoffFlag, blockBackoffDefault, desc)
-	viper.SetDefault(blockBackoffViperKey, blockBackoffDefault)
 	viper.BindPFlag(blockBackoffViperKey, f.Lookup(blockBackoffFlag))
-	viper.BindEnv(blockBackoffViperKey, blockBackoffEnv)
 }
 
 var (
@@ -111,15 +112,13 @@ func BlockLimit(f *pflag.FlagSet) {
 	desc := fmt.Sprintf(`Limit number of block that can be prefetched while listening
 Environment variable: %q`, blockLimitEnv)
 	f.Int64(blockLimitFlag, blockLimitDefault, desc)
-	viper.SetDefault(blockLimitViperKey, blockLimitDefault)
 	viper.BindPFlag(blockLimitViperKey, f.Lookup(blockLimitFlag))
-	viper.BindEnv(blockLimitViperKey, blockLimitEnv)
 }
 
 var (
 	trackerDepthFlag     = "listener-tracker-depth"
 	trackerDepthViperKey = "listener.tracker.depth"
-	trackerDepthDefault  = int64(5)
+	trackerDepthDefault  = int64(0)
 	trackerDepthEnv      = "LISTENER_TRACKER_DEPTH"
 )
 
@@ -128,7 +127,5 @@ func TrackerDepth(f *pflag.FlagSet) {
 	desc := fmt.Sprintf(`Depth at which we consider a block final (to avoid falling into a re-org)
 Environment variable: %q`, trackerDepthEnv)
 	f.Int64(trackerDepthFlag, trackerDepthDefault, desc)
-	viper.SetDefault(trackerDepthViperKey, trackerDepthDefault)
 	viper.BindPFlag(trackerDepthViperKey, f.Lookup(trackerDepthFlag))
-	viper.BindEnv(trackerDepthViperKey, trackerDepthEnv)
 }
