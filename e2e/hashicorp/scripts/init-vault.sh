caa7a275d0d7963e494d9d8dc4c5989ef75d43a5
alexandre.belling <alexandre.belling@consensys.net>
2019-04-16 20:01:42 +0200

    feature(e2e): Clean end-to-end testing with vault

---
+++ e2e/hashicorp/scripts/init-vault.sh
@@ -0,0 +1,19 @@
+# Set the VAULT_ADDRESS
+export VAULT_ADDR=http://127.0.0.1:8200
+
+# INIT the vault
+curl --request POST --data '{"secret_shares": 1, "secret_threshold": 1}' ${VAULT_ADDR}/v1/sys/init > init.json
+
+# UNSEAL the vault
+export VAULT_UNSEAL_KEY=$(cat init.json | jq .keys | jq .[0])
+curl --request POST --data '{"key": '${VAULT_UNSEAL_KEY}'}' ${VAULT_ADDR}/v1/sys/unseal
+
+# Set the VAULT_TOKEN as environment variable
+export VAULT_TOKEN=$(cat init.json | jq .root_token | tr -d '"')
+
+# Enable secret engine
+curl --header "X-Vault-Token: ${VAULT_TOKEN}" --request POST \
+     --data '{"type": "kv", "config": {"force_no_cache": true} }' \
+    ${VAULT_ADDR}/v1/sys/mounts/secret
+
+rm init.json
---
+++ e2e/hashicorp/scripts/run-test.sh
@@ -0,0 +1,14 @@
+# Hard restart the vault if necessary
+docker-compose -f ../docker-compose.yml down | true
+docker-compose -f ../docker-compose.yml up -d --force-recreate
+sleep 2
+
+# Unseal vault and the propers environment variables
+source init-vault.sh
+
+# Run the tests passing the variables to the go command
+VAULT_TOKEN=${VAULT_TOKEN} VAULT_ADDR=${VAULT_ADDR} \
+    go test ../../../secretstore/hashicorp
+
+# Cleanly shut down the vault container
+docker-compose -f ../docker-compose.yml down
