6e6807cf4f7ecb1280a34098c56808a0cca8cf54
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-18 09:34:11 +0200

    feat@abi(exported.go): implement dependency injection pattern

---
+++ abi/exported_test.go
@@ -0,0 +1,18 @@
+package abi
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInitRegistry(t *testing.T) {
+	viper.Set("abis", "ERC20[v0.1.3]:[{\"constant\":true,\"inputs\":[],\"name\":\"myFunction\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"stateMutability\":\"view\",\"type\":\"function\"}]")
+	InitRegistry()
+
+	m, err := GlobalRegistry().GetMethodByID("myFunction@ERC20[v0.1.3]")
+	assert.NotNil(t, m, "Method should be available")
+	assert.Nil(t, err, "Should not error")
+}
