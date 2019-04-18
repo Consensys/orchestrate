f1b72af63dfc534326210b8c3b27e663b909ecdd
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-18 09:33:53 +0200

    refactor@abi(types.go): declare interface in abi

---
+++ abi/types.go
@@ -0,0 +1,28 @@
+package abi
+
+import (
+	ethabi "github.com/ethereum/go-ethereum/accounts/abi"
+	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/protos/abi"
+)
+
+// Registry is an interface to manage ABIs
+type Registry interface {
+	// Retrieve method using unique identifier
+	GetMethodByID(ID string) (ethabi.Method, error)
+	// Retrieve method using 4 bytes signature
+	GetMethodBySig(sig string) (ethabi.Method, error)
+	// Retrieve event using unique identifier
+	GetEventByID(ID string) (ethabi.Event, error)
+	// Retrieve event using 4 bytes signature
+	GetEventBySig(sig string) (ethabi.Event, error)
+	// Register a new contract in the ABI
+	RegisterContract(contract *abi.Contract) error
+	// Returns the new bytecode
+	GetBytecodeByID(id string) ([]byte, error)
+}
+
+// Crafter takes a method abi and args to craft a transaction
+type Crafter interface {
+	Craft(method ethabi.Method, args ...string) ([]byte, error)
+	CraftConstructor(method ethabi.Method, args ...string) ([]byte, error)
+}
