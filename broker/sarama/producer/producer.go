5c6f127db1fca3a072711b4a625dee5027580b23
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-19 10:39:10 +0200

    feat@broker(producer): implement producer HandlerFUnc

---
+++ broker/sarama/producer/producer.go
@@ -0,0 +1,45 @@
+package producer
+
+import (
+	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
+	encoding "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/encoding/sarama"
+	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/engine"
+)
+
+func init() {
+	marshaller = encoding.NewMarshaller()
+}
+
+var marshaller *encoding.Marshaller
+
+// PrepareMsg function should prepare a sarama.ProducerMessage from a TxContext
+type PrepareMsg func(*engine.TxContext, *sarama.ProducerMessage) error
+
+// Producer creates a producer handler
+func Producer(p sarama.SyncProducer, prepareMsg PrepareMsg) engine.HandlerFunc {
+	return func(txctx *engine.TxContext) {
+		// Prepare Message
+		msg := &sarama.ProducerMessage{}
+		err := prepareMsg(txctx, msg)
+		if err != nil {
+			txctx.AbortWithError(err)
+			txctx.Logger.WithError(err).Errorf("producer: could not prepare message")
+			return
+		}
+
+		// Send message
+		partition, offset, err := p.SendMessage(msg)
+		if err != nil {
+			txctx.AbortWithError(err)
+			txctx.Logger.WithError(err).Errorf("producer: could not produce message")
+			return
+		}
+
+		txctx.Logger.WithFields(log.Fields{
+			"kafka.out.partition": partition,
+			"kafka.out.offset":    offset,
+			"kafka.out.topic":     msg.Topic,
+		}).Tracef("producer: message produced")
+	}
+}
