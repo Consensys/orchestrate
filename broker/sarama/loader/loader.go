f408b3c637e85618c4bb21e8b14a0a6295ba093f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-19 10:24:56 +0200

    feat@broker/sarama(loader): implement a loader based on sarama

---
+++ broker/sarama/loader/loader.go
@@ -0,0 +1,36 @@
+package loader
+
+import (
+	"github.com/Shopify/sarama"
+	log "github.com/sirupsen/logrus"
+	encoding "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/encoding/sarama"
+	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/engine"
+)
+
+func init() {
+	unmarshaller = encoding.NewUnmarshaller()
+}
+
+var unmarshaller *encoding.Unmarshaller
+
+// Loader is a Middleware enginer.HandlerFunc that Load sarama.ConsumerGroup messages
+func Loader(txctx *engine.TxContext) {
+	// Unmarshal message
+	err := unmarshaller.Unmarshal(txctx.Msg, txctx.Envelope)
+	if err != nil {
+		// TODO: handle error
+		txctx.Logger.Errorf("Error unmarshalling: %v", err)
+		txctx.AbortWithError(err)
+		return
+	}
+
+	// Enrich Logger
+	msg := txctx.Msg.(*sarama.ConsumerMessage)
+	txctx.Logger = txctx.Logger.WithFields(log.Fields{
+		"kafka.in.topic":     msg.Topic,
+		"kafka.in.offset":    msg.Offset,
+		"kafka.in.partition": msg.Partition,
+	})
+
+	txctx.Logger.Debugf("Message loaded:%v", txctx.Envelope.String())
+}
