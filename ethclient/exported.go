70690ce90bd455eba0c0ff6a5fd25e12643948ba
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-20 17:38:33 +0200

    fix@ethclien(exported.go): ensure Init is called only once

--- ethclient/exported.go
+++ ethclient/exported.go
@@ -2,6 +2,7 @@ package ethclient
 
 import (
 	"context"
+	"sync"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
@@ -11,7 +12,10 @@ func init() {
 	mec = NewMultiClient()
 }
 
-var mec *MultiClient
+var (
+	mec      *MultiClient
+	initOnce = sync.Once{}
+)
 
 // Init initialize Dials chains
 //
@@ -19,20 +23,21 @@ var mec *MultiClient
 // Cancelling input Context will stop multiclient
 // If an error occurs during initialization, it will panic
 func Init(ctx context.Context) {
-	// Dial Ethereum client (URLs found in viper configuration)
-	err := mec.MultiDial(ctx, viper.GetStringSlice(urlViperKey))
-	if err != nil {
-		log.WithError(err).Fatalf("ethereum: could not dial multi-client")
-	}
-	chainIDs := mec.Networks(ctx)
-	log.Infof("ethereum: multi-client ready (connected to chains: %v)", chainIDs)
+	initOnce.Do(func() {
+		// Dial Ethereum client (URLs found in viper configuration)
+		err := mec.MultiDial(ctx, viper.GetStringSlice(urlViperKey))
+		if err != nil {
+			log.WithError(err).Fatalf("ethereum: could not dial multi-client")
+		}
+		chainIDs := mec.Networks(ctx)
+		log.Infof("ethereum: multi-client ready (connected to chains: %v)", chainIDs)
 
-	// Wait for context to be done and then close
-	go func() {
-		<-ctx.Done()
-		mec.Close()
-	}()
-
+		// Wait for context to be done and then close
+		go func() {
+			<-ctx.Done()
+			mec.Close()
+		}()
+	})
 }
 
 // GlobalMultiClient returns global MultiClient
