f6fb6e4f98e8ebec2087455deb58f54391753ccc
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-20 23:11:16 +0200

    feat(faucet): implement faucet interface

--- faucet/faucet.go
+++ faucet/faucet.go
@@ -4,24 +4,16 @@ import (
 	"context"
 	"math/big"
 
-	ethcommon "github.com/ethereum/go-ethereum/common"
+	"gitlab.com/ConsenSys/client/fr/core-stack/infra/faucet.git/types"
 )
 
-// Request holds information for a Faucet Credit Request
-type Request struct {
-	ChainID     *big.Int
-	Creditor    ethcommon.Address
-	Beneficiary ethcommon.Address
-	Amount      *big.Int
-}
-
 // CreditFunc are functions expected to trigger an credit ether
-type CreditFunc func(ctx context.Context, r *Request) (*big.Int, bool, error)
+type CreditFunc func(ctx context.Context, r *types.Request) (*big.Int, bool, error)
 
 // Faucet is an interface for crediting an account with ether
 type Faucet interface {
 	// Credit should credit an account based on its own set of security rules
 	// If credit is successful it should return amount credited and true
 	// Credit should respond synchronously (not wait for a credit transaction to be mined)
-	Credit(ctx context.Context, r *Request) (*big.Int, bool, error)
+	Credit(ctx context.Context, r *types.Request) (*big.Int, bool, error)
 }
