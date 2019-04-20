47c0d607da71c7f69b4026013a75af0804111822
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-20 23:11:31 +0200

    feat@faucet(ock): implement mock faucet

--- faucet/mock/faucet.go
+++ faucet/mock/faucet.go
@@ -4,18 +4,23 @@ import (
 	"context"
 	"math/big"
 
-	"gitlab.com/ConsenSys/client/fr/core-stack/infra/faucet.git/faucet"
+	"gitlab.com/ConsenSys/client/fr/core-stack/infra/faucet.git/types"
 )
 
 // Credit is a Mock crediting function
-func Credit(ctx context.Context, r *faucet.Request) (*big.Int, bool, error) {
+func Credit(ctx context.Context, r *types.Request) (*big.Int, bool, error) {
 	return r.Amount, true, nil
 }
 
 // Faucet is a mock Faucet
 type Faucet struct{}
 
+// NewFaucet creates a new mock faucet
+func NewFaucet() *Faucet {
+	return &Faucet{}
+}
+
 // Credit is mock crediting function
-func (faucet *Faucet) Credit(ctx context.Context, r *faucet.Request) (*big.Int, bool, error) {
+func (faucet *Faucet) Credit(ctx context.Context, r *types.Request) (*big.Int, bool, error) {
 	return Credit(ctx, r)
 }
