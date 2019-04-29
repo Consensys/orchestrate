adf90ebfd7a0a5ea6593eff4f45102038c850230
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-04-30 00:30:32 +0200

    style(): remove deprecated comment

--- handlers/logger/logger.go
+++ handlers/logger/logger.go
@@ -10,10 +10,6 @@ import (
 
 // Logger creates a handler middleware that log basic information about tx execution
 func Logger(txctx *engine.TxContext) {
-	// TODO: logger enrichment should append in Loader middleware, so we remove dependency to sarama in Logger
-	// msg := txctx.Msg.(*sarama.ConsumerMessage)
-	// txctx.Logger = log.WithFields(infsarama.ConsumerMessageFields(msg))
-
 	txctx.Logger.Trace("logger: new message")
 	start := time.Now()
 
