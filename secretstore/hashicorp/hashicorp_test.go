dd9ef3b89dcebcde55f9460d30d91eef428f7739
christian.tran <christian.tran@consensys.net>
2019-05-06 19:25:53 +0200

    fix(ci): add hashicorp integration test

--- secretstore/hashicorp/hashicorp_test.go
+++ secretstore/hashicorp/hashicorp_test.go
@@ -1,26 +1,26 @@
 package hashicorp
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/suite"
-// 	"gitlab.com/ConsenSys/client/fr/core-stack/infra/key-store.git/secretstore/testutils"
-// )
+	"github.com/stretchr/testify/suite"
+	"gitlab.com/ConsenSys/client/fr/core-stack/infra/key-store.git/secretstore/testutils"
+)
 
-// type HashicorpKeyStoreTestSuite struct {
-// 	testutils.SecretStoreTestSuite
-// }
+type HashicorpKeyStoreTestSuite struct {
+	testutils.SecretStoreTestSuite
+}
 
-// func (s *HashicorpKeyStoreTestSuite) SetupTest() {
-// 	config := NewConfig()
-// 	hashicorps, err := NewHashiCorp(config)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	s.Store = hashicorps
-// }
+func (s *HashicorpKeyStoreTestSuite) SetupTest() {
+	config := NewConfig()
+	hashicorps, err := NewHashiCorp(config)
+	if err != nil {
+		panic(err)
+	}
+	s.Store = hashicorps
+}
 
-// func TestMock(t *testing.T) {
-// 	s := new(HashicorpKeyStoreTestSuite)
-// 	suite.Run(t, s)
-// }
+func TestMock(t *testing.T) {
+	s := new(HashicorpKeyStoreTestSuite)
+	suite.Run(t, s)
+}
