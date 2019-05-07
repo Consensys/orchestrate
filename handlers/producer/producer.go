37984c93da5dbc941a7695c93cfbf1675d274b2c
Julien Marchand <julien.marchand@consensys.net>
2019-05-07 17:56:49 +0000

    Feature/multi producer

--- handlers/producer/producer.go
+++ handlers/producer/producer.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Shopify/sarama"
 	log "github.com/sirupsen/logrus"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/engine"
+	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/types/envelope"
 )
 
 // PrepareMsg function should prepare a sarama.ProducerMessage from a TxContext
@@ -37,5 +38,25 @@ func Producer(p sarama.SyncProducer, prepareMsg PrepareMsg) engine.HandlerFunc {
 			"kafka.out.topic":     msg.Topic,
 		})
 		txctx.Logger.Tracef("producer: message produced")
+	}
+}
+
+// MultiProducer creates a multi producer handler
+func MultiProducer(p sarama.SyncProducer, prepareMsg PrepareMsg) engine.HandlerFunc {
+	return func(txctx *engine.TxContext) {
+		txctx.Next()
+
+		envelopes := txctx.Get("envelopes").([]*envelope.Envelope)
+		for _, e := range envelopes {
+			// Prepare Message
+			subTxctx := &engine.TxContext{
+				Envelope: e,
+				Logger:   txctx.Logger,
+			}
+			subTxctx.WithContext(txctx.Context())
+			subTxctx.Set("envelopes", nil)
+
+			Producer(p, prepareMsg)(subTxctx)
+		}
 	}
 }
