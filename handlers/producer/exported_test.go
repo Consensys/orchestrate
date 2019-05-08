7dc071428b9a22b23d031b4eb181e3369a7a2c15
christian.tran <christian.tran@consensys.net>
2019-05-08 11:53:38 +0200

    fix:remove exported producer

--- handlers/producer/exported_test.go
+++ handlers/producer/exported_test.go
@@ -1,13 +1,13 @@
 package producer
 
-import (
-	"context"
-	"testing"
+// import (
+// 	"context"
+// 	"testing"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestInit(t *testing.T) {
-	Init(context.Background())
-	assert.NotNil(t, handler, "Global handler should have been set")
-}
+// func TestInit(t *testing.T) {
+// 	Init(context.Background())
+// 	assert.NotNil(t, handler, "Global handler should have been set")
+// }
