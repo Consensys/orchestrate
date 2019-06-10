3e0784582e4ba137d7e8ae722b606f105d9da95e
alexandre.belling <alexandre.belling@consensys.net>
2019-06-10 18:08:08 +0200

    lint(fix): Fix lint to pass the CI

--- engine/composite.go
+++ engine/composite.go
@@ -1,11 +1,10 @@
-
 package engine
 
 import (
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/common"
 )
 
-// MatcherFunc is an abstract type for a class of function 
+// MatcherFunc is an abstract type for a class of function
 // used to determine if a handler is appropriate to use in a Composite
 type MatcherFunc func(txctx *TxContext) bool
 
@@ -14,15 +13,15 @@ func MatchAll(txctx *TxContext) bool { return true }
 
 // MatchableHandler is a sugar type to manipulate a handler and its matcher in the same object
 type MatchableHandler struct {
-	Handler		HandlerFunc
-	Matcher 	MatcherFunc
+	Handler HandlerFunc
+	Matcher MatcherFunc
 }
 
 // NewMatchableHandler creates an explicit matchable handler from a given handler and matcher
 func NewMatchableHandler(handler HandlerFunc, matcher MatcherFunc) MatchableHandler {
 	return MatchableHandler{
-		Handler:	handler,
-		Matcher:	matcher,
+		Handler: handler,
+		Matcher: matcher,
 	}
 }
 
@@ -30,24 +29,24 @@ func NewMatchableHandler(handler HandlerFunc, matcher MatcherFunc) MatchableHand
 // The executed handler is the first one to be matched. Tests starts at rank 0.
 type CompositeHandler struct {
 	// chain of handlers to be to be tested for execution
-	matchableHandlers 	[]MatchableHandler
+	matchableHandlers []MatchableHandler
 }
 
 // NewCompositeHandler creates a new composite handler. Accept zero argument calls
 func NewCompositeHandler(matchableHandlers ...MatchableHandler) CompositeHandler {
-	return CompositeHandler{ matchableHandlers: matchableHandlers }
+	return CompositeHandler{matchableHandlers: matchableHandlers}
 }
 
 // Registers append a new handler and matcher to the composite
 func (c *CompositeHandler) Registers(handler HandlerFunc, matcher MatcherFunc) {
-	h := NewMatchableHandler(handler, matcher) 
+	h := NewMatchableHandler(handler, matcher)
 	c.matchableHandlers = append(
 		c.matchableHandlers,
 		h,
 	)
 }
 
-// FirstMatchFunc returns a handler executing the first handler to match the context 
+// FirstMatchFunc returns a handler executing the first handler to match the context
 func (c *CompositeHandler) FirstMatchFunc() HandlerFunc {
 	return func(txctx *TxContext) {
 		for _, matchable := range c.matchableHandlers {
@@ -74,13 +73,11 @@ func (c *CompositeHandler) AllMatchSeqFunc() HandlerFunc {
 func (c *CompositeHandler) AllMatchParallelFunc() HandlerFunc {
 	return func(txctx *TxContext) {
 		matched := []func(){}
-
 		for _, matchable := range c.matchableHandlers {
 			if matchable.Matcher(txctx) {
-				matched = append(matched, func() { matchable.Handler(txctx)})
+				matched = append(matched, func() { matchable.Handler(txctx) })
 			}
 		}
 		common.InParallel(matched...)
 	}
 }
-
