b3209a964417f7b7bfaef0edd1b110b3aebf9d7f
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-06-29 17:05:22 +0200

    test@utils(keys): fix test

--- utils/keys_test.go
+++ utils/keys_test.go
@@ -21,8 +21,7 @@ func TestChainAccountKey(t *testing.T) {
 	)
 
 	chainID, acc, err := FromChainAccountKey("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C@3")
-
-	assert.NoError(t, err, "Should not error")
+	assert.Nil(t, err, "FromChainAccountKey should not error")
 	assert.Equal(t, int64(3), chainID.Int64(), "ChainID should be correct")
 	assert.Equal(t, "0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C", acc.Hex(), "Account should be correct")
 
