f838164af572999b2fed38afb5416d1de65a0deb
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-07-05 10:48:09 +0200

    fix@errors(isErrorClass): fix computation

--- errors/error.go
+++ errors/error.go
@@ -40,5 +40,5 @@ func isErrorClass(code, base uint64) bool {
 	// Error codes have a 5 hex reprensentation (<=> 20 bits representation)
 	//  - (code^base)&255<<12 compute difference between 2 first nibbles (bits 13 to 20)
 	//  - (code^base)&(base&15<<8) compute difference between 3rd nibble in case base 3rd nibble is non zero (bits 9 to 12)
-	return (code^base)&(255<<12+base&15<<8) == 0
+	return (code^base)&(255<<12+15<<8&base) == 0
 }
