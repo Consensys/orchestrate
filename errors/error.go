b936e2848e017ccc5fc2df108c85a10f363842c5
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-07-05 10:39:16 +0200

    doc@errors(isErrorClass): document isErrorClass

--- errors/error.go
+++ errors/error.go
@@ -30,6 +30,15 @@ func FromError(err error) *ierror.Error {
 }
 
 // isErrorClass returns whether code belongs to a base error class
+//
+// While codes are uint64 for performance purposes they should be seen as 5 nibble codes encoded on 20 bits
+//
+// For code to be of class base
+//  - 2 first nibbles must be identical (e.g. DB300 belongs to class DB000 but DB300 doesn't belong to 9F000)
+//  - if base 3rd nibble is non zero then 3rd nibble must be identical (e.g. DB201 belongs to DB200 but DB300 doesn't belong to DB200)
 func isErrorClass(code, base uint64) bool {
-	return (base^code)&(255<<12+15<<8&base) == 0
+	// Error codes have a 5 hex reprensentation (<=> 20 bits representation)
+	//  - (code^base)&255<<12 compute difference between 2 first nibbles (bits 13 to 20)
+	//  - (code^base)&(base&15<<8) compute difference between 3rd nibble in case base 3rd nibble is non zero (bits 9 to 12)
+	return (code^base)&(255<<12+base&15<<8) == 0
 }
