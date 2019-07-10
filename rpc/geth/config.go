aa22c1c91035e075f2916e91e8e67991d71b168a
Julien Marchand <julien.marchand@consensys.net>
2019-07-10 14:47:19 +0000

    feat(ethclient): add max elapsed time for call

--- rpc/geth/config.go
+++ rpc/geth/config.go
@@ -1,8 +1,10 @@
 package geth
 
 import (
+	"fmt"
 	"time"
 
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
@@ -10,8 +12,24 @@ func init() {
 	viper.SetDefault("ethclient.retry.initinterval", 500*time.Millisecond)
 	viper.SetDefault("ethclient.retry.randomfactor", 0.5)
 	viper.SetDefault("ethclient.retry.multiplier", 1.5)
-	viper.SetDefault("ethclient.retry.maxinterval", 10*time.Second)
-	viper.SetDefault("ethclient.retry.maxelapsedtime", 60*time.Second)
+	viper.SetDefault("ethclient.retry.maxinterval", 30*time.Second)
+	_ = viper.BindEnv(maxElapsedTimeViperKey, maxElapsedTimeEnv)
+	viper.SetDefault(maxElapsedTimeViperKey, maxElapsedTimeDefault)
+}
+
+var (
+	maxElapsedTimeFlag     = "ethclient-retry-maxelapsedtime"
+	maxElapsedTimeViperKey = "ethclient.retry.maxelapsedtime"
+	maxElapsedTimeDefault  = 1 * time.Hour
+	maxElapsedTimeEnv      = "ETH_CLIENT_RETRY_MAX_ELAPSED_TIME"
+)
+
+// MaxElapsedTime register flag for maximum elapsed time to retry RPC calls on Ethereum clients
+func MaxElapsedTime(f *pflag.FlagSet) {
+	desc := fmt.Sprintf(`Max elapsed time to retry rpc calls on Ethereum clients
+Environment variable: %q`, maxElapsedTimeEnv)
+	f.Duration(maxElapsedTimeFlag, maxElapsedTimeDefault, desc)
+	_ = viper.BindPFlag(maxElapsedTimeViperKey, f.Lookup(maxElapsedTimeFlag))
 }
 
 // TODO: move everything related to Retry and Backoff to pkg
@@ -33,7 +51,7 @@ func NewRetryConfig() *RetryConfig {
 	config.RandomizationFactor = viper.GetFloat64("ethclient.retry.randomfactor")
 	config.Multiplier = viper.GetFloat64("ethclient.retry.multiplier")
 	config.MaxInterval = viper.GetDuration("ethclient.retry.maxinterval")
-	config.MaxElapsedTime = viper.GetDuration("ethclient.retry.maxelapsedtime")
+	config.MaxElapsedTime = viper.GetDuration(maxElapsedTimeViperKey)
 
 	return config
 }
