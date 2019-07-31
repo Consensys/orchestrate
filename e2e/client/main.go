4e0fdc6c427edce8d91acc67cfe97b06cafaf008
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2019-07-31 22:48:33 +0200

    test@e2e(client): implement an end-to-end test using new client declaration

--- e2e/client/main.go
+++ e2e/client/main.go
@@ -2,78 +2,60 @@ package main
 
 import (
 	"context"
-	"math/big"
 
-	ethcommon "github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/golang/protobuf/ptypes"
 	log "github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
 
-	store "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/services/envelope-store"
+	evlpstore "gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/services/envelope-store"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/types/chain"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/types/envelope"
 	"gitlab.com/ConsenSys/client/fr/core-stack/pkg.git/types/ethereum"
+	"gitlab.com/ConsenSys/client/fr/core-stack/service/envelope-store.git/client"
 )
 
 func main() {
-	conn, err := grpc.DialContext(
-		context.Background(),
-		"localhost:8080",
-		grpc.WithInsecure(),
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
-	client := store.NewStoreClient(conn)
+	client.Init(context.Background())
+	defer client.Close()
 
-	txData := (&ethereum.TxData{}).
-		SetNonce(10).
-		SetTo(ethcommon.HexToAddress("0xAf84242d70aE9D268E2bE3616ED497BA28A7b62C")).
-		SetValue(big.NewInt(100000)).
-		SetGas(2000).
-		SetGasPrice(big.NewInt(200000)).
-		SetData(hexutil.MustDecode("0xabcd"))
-
-	tr := &envelope.Envelope{
+	// Store envelope
+	evlp := &envelope.Envelope{
 		Chain:    chain.CreateChainInt(888),
-		Metadata: &envelope.Metadata{Id: "6be0-bc19-900b-1ef8-bb6d-61b9-ad38-ba11"},
+		Metadata: &envelope.Metadata{Id: "6be0-bc19-900b-1ef8-bb6d-61b9-ad38-ba12"},
 		Tx: &ethereum.Transaction{
-			TxData: txData,
-			Raw:    ethereum.HexToData("0xf86c0184ee6b2800a2529094ff778b716fc07d98839f48ddb88d8be583beb684872386f26fc1000082abcd29a0d1139ca4c70345d16e00f624622ac85458d450e238a48744f419f5345c5ce562a05bd43c512fcaf79e1756b2015fec966419d34d2a87d867b9618a48eca33a1a80"),
-			Hash:   ethereum.HexToHash("0x6a0caf026cb1f012abe19e9e02c53f23713b0033d7a72e534136104b5447a210"),
+			Raw:  ethereum.HexToData("0xf86c0184ee6b2800a2529094ff778b716fc07d98839f48ddb88d8be583beb684872386f26fc1000082abcd29a0d1139ca4c70345d16e00f624622ac85458d450e238a48744f419f5345c5ce562a05bd43c512fcaf79e1756b2015fec966419d34d2a87d867b9618a48eca33a1a80"),
+			Hash: ethereum.HexToHash("0x6a0caf026cb1f012abe19e9e02c53f23713b0033d7a72e534136104b5447a21a"),
 		},
 	}
-
-	resp, err := client.Store(context.Background(), &store.StoreRequest{
-		Envelope: tr,
-	})
+	resp, err := client.GlobalEnvelopeStoreClient().Store(
+		context.Background(),
+		&evlpstore.StoreRequest{
+			Envelope: evlp,
+		})
 	if err != nil {
 		log.WithError(err).Errorf("Could not store")
 	}
 
-	timestamp, err := ptypes.Timestamp(resp.LastUpdated)
+	timestamp, err := ptypes.Timestamp(resp.GetLastUpdated())
 	if err != nil {
-		log.WithError(err).Errorf("Could not store")
+		log.WithError(err).Errorf("Could not retrieve timestamp")
 	}
 
 	log.WithFields(log.Fields{
-		"status": resp.Status,
+		"status": resp.GetStatus(),
 		"at":     timestamp,
 	}).Infof("Envelope stored")
 
-	res, _ := client.LoadByTxHash(context.Background(), &store.TxHashRequest{
-		ChainId: tr.GetChain(),
-		TxHash:  tr.GetTx().GetHash().Hex(),
-	})
-
-	log.Println(res.GetStatus())
-	log.Println(res.GetLastUpdated())
-	log.Println(res.GetEnvelope().GetChain())
-	log.Println(res.GetErr())
+	res, err := client.GlobalEnvelopeStoreClient().LoadByTxHash(
+		context.Background(),
+		&evlpstore.TxHashRequest{
+			ChainId: evlp.GetChain(),
+			TxHash:  evlp.GetTx().GetHash().Hex(),
+		})
 	if err != nil {
-		log.WithError(err).Errorf("Could not load")
+		log.WithError(err).Errorf("Could not load envelope")
 	}
-
-	conn.Close()
+	log.WithFields(log.Fields{
+		"status":   res.GetStatus(),
+		"chain.id": res.GetEnvelope().GetChain().ID().String(),
+	}).Infof("Envelope loaded")
 }
