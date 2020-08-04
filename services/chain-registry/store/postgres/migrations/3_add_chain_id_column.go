4b4f18b07c464f05d42cdd965d3edde6481ff460
gabriel.garrido <gabriel.garrido@consensys.net>
2020-08-04 16:46:47 +0200

    fix: migrate chains for compatibility with v2.3.0

--- services/chain-registry/store/postgres/migrations/3_add_chain_id_column.go
+++ services/chain-registry/store/postgres/migrations/3_add_chain_id_column.go
@@ -1,8 +1,13 @@
 package migrations
 
 import (
+	"context"
+
 	"github.com/go-pg/migrations/v7"
 	log "github.com/sirupsen/logrus"
+	"gitlab.com/ConsenSys/client/fr/core-stack/orchestrate.git/pkg/ethereum/ethclient/rpc"
+	"gitlab.com/ConsenSys/client/fr/core-stack/orchestrate.git/services/chain-registry/chain-registry/utils"
+	"gitlab.com/ConsenSys/client/fr/core-stack/orchestrate.git/services/chain-registry/store/models"
 )
 
 func addChainIDColumn(db migrations.DB) error {
@@ -10,7 +15,17 @@ func addChainIDColumn(db migrations.DB) error {
 
 	_, err := db.Exec(`
 ALTER TABLE chains
-	ADD COLUMN chain_id BIGINT NOT NULL;
+	ALTER COLUMN listener_depth SET DEFAULT 0,
+	ALTER COLUMN listener_current_block SET DEFAULT 0,
+	ALTER COLUMN listener_starting_block SET DEFAULT 0;
+
+ALTER TABLE chains
+	ALTER COLUMN listener_depth SET NOT NULL,
+	ALTER COLUMN listener_current_block SET NOT NULL,
+	ALTER COLUMN listener_starting_block SET NOT NULL;
+
+ALTER TABLE chains
+	ADD COLUMN chain_id BIGINT NOT NULL DEFAULT 0;
 	`)
 
 	if err != nil {
@@ -18,6 +33,11 @@ ALTER TABLE chains
 	}
 
 	log.Infof("Added chainID columns on table %q", "chains")
+
+	err = updateChainIDs(context.Background(), db)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
@@ -28,6 +48,16 @@ func dropChainIDColumn(db migrations.DB) error {
 	_, err := db.Exec(`
 ALTER TABLE chains 
 	DROP COLUMN chain_id;
+
+ALTER TABLE chains
+	ALTER COLUMN listener_depth DROP DEFAULT,
+	ALTER COLUMN listener_current_block DROP DEFAULT,
+	ALTER COLUMN listener_starting_block DROP DEFAULT;
+
+ALTER TABLE chains
+	ALTER COLUMN listener_depth DROP NOT NULL,
+	ALTER COLUMN listener_current_block DROP NOT NULL,
+	ALTER COLUMN listener_starting_block DROP NOT NULL;
 	`)
 
 	if err != nil {
@@ -40,3 +70,37 @@ ALTER TABLE chains
 }
 
 func init() { Collection.MustRegisterTx(addChainIDColumn, dropChainIDColumn) }
+
+func updateChainIDs(ctx context.Context, db migrations.DB) error {
+	rpc.Init(ctx)
+	ec := rpc.GlobalClient()
+
+	log.Debugf("fetching chainIDs from rpc nodes")
+
+	var chains []*models.Chain
+	err := db.Model(&chains).Where(`chain_id = ?`, 0).Select()
+	if err != nil {
+		return err
+	}
+
+	for _, chain := range chains {
+		chainID, err := utils.GetChainID(ctx, ec, chain.URLs)
+		if err != nil {
+			return err
+		}
+
+		_, err = db.Model(&models.Chain{ChainID: chainID.String()}).
+			Where("uuid = ?", chain.UUID).UpdateNotZero()
+
+		if err != nil {
+			return err
+		}
+
+		log.WithField("chainName", chain.Name).
+			WithField("chainUUID", chain.UUID).
+			WithField("chainID", chainID).
+			Infof("chain was updated correctly")
+	}
+
+	return nil
+}
