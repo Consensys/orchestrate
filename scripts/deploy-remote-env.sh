4daed3d016d82403cba21f6f01948db0bd0bd6be
Dario Anongba Varela <dario.varela@consensys.net>
2021-02-16 19:07:42 +0100

    fix: CI pipeline on tags improvements (#519)

--- scripts/deploy-remote-env.sh
+++ scripts/deploy-remote-env.sh
@@ -12,13 +12,10 @@ echo "Orchestrate namespace: $ORCHESTRATE_NAMESPACE"
 PARAMETERS=""
 [ "$ORCHESTRATE_NAMESPACE" ] && export PARAMETERS=$PARAMETERS,\"orchestrate_namespace\":\"$ORCHESTRATE_NAMESPACE\"
 [ "$ORCHESTRATE_TAG" ] && export PARAMETERS=$PARAMETERS,\"orchestrate_tag\":\"$ORCHESTRATE_TAG\"
-[ "$TEST_TAG" ] && export PARAMETERS=$PARAMETERS,\"test_tag\":\"$TEST_TAG\"
 [ "$ORCHESTRATE_REPOSITORY" ] && export PARAMETERS=$PARAMETERS,\"orchestrate_repository\":\"$ORCHESTRATE_REPOSITORY\"
-[ "$TEST_REPOSITORY" ] && export PARAMETERS=$PARAMETERS,\"test_repository\":\"$TEST_REPOSITORY\"
 [ "$REGISTRY_URL" ] && export PARAMETERS=$PARAMETERS,\"registry_url\":\"$REGISTRY_URL\"
 [ "$REGISTRY_USERNAME" ] && export PARAMETERS=$PARAMETERS,\"registry_username\":\"$REGISTRY_USERNAME\"
 [ "$REGISTRY_PASSWORD" ] && export PARAMETERS=$PARAMETERS,\"registry_password\":\"$REGISTRY_PASSWORD\"
-[ "$RUN_STRESS_TEST" ] && export PARAMETERS=$PARAMETERS,\"run_stress_test\":$RUN_STRESS_TEST
 [ "$ENVIRONMENT_VALUES" ] && export PARAMETERS=$PARAMETERS,\"environment_values\":\"$ENVIRONMENT_VALUES\"
 [ "$PARAMETERS" ] && PARAMETERS=${PARAMETERS:1}
 
