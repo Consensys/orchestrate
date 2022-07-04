319c291cfa6cb6445009e4937f505b381d303899
Thiên Toán <toanalien@gmail.com>
2022-07-04 10:45:10 +0700

    Update config.go

--- pkg/toolkit/app/auth/key/config.go
+++ pkg/toolkit/app/auth/key/config.go
@@ -26,7 +26,7 @@ func Flags(f *pflag.FlagSet) {
 
 // apiKey register flag for Authentication with API Key
 func apiKey(f *pflag.FlagSet) {
-	desc := fmt.Sprintf(`Key used for authentication (it should be used only for Orchestrate internal authenetication).
+	desc := fmt.Sprintf(`Key used for authentication (it should be used only for Orchestrate internal authentication).
 Environment variable: %q`, apiKeyEnv)
 	f.String(apiKeyFlag, apiKeyDefault, desc)
 	_ = viper.BindPFlag(APIKeyViperKey, f.Lookup(apiKeyFlag))
